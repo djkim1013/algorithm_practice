1e72b1060f9293289efeb2e19e0bab25a9672f9d
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-05-05 03:13:29 +0900

    210505 algospot
    JLIS

---
+++ algo/210000_form.cpp
@@ -0,0 +1,22 @@
+/*
+AOJ : link
+2021.##.##
+문제 분석 : 
+해결 분석 : 
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int C=0;
+    cin>>C;
+    for (int i=0;i<C;i++)
+    {
+        
+    }
+    return 0;
+}
---
+++ algo/2105/2100505_jlis.cpp
@@ -0,0 +1,51 @@
+/*
+JLIS : https://algospot.com/judge/problem/read/JLIS
+2021.05.05
+문제 분석 : 동적계획법
+해결 분석 : 각각의 수열에서 각 요소에서 시작하는 가장 긴 부분증가수열을 구하는 함수를
+        만든 후 탐욕법을 사용하려 했으나, 같은 요소에서 시작하지만 서로 다른 부분수열
+        이 저장 되지 않는 문제가 발생하였고, 이 방식대로 오류가 나지 않게끔 수저오하면
+        중복이 발생하는 탐욕법과 같아진다. 교재의 방식대로 구현하여 해결하였다.
+*/
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const long NEGI=numeric_limits<long>::min();
+int n,m;
+
+int find_JLIS(vector<int>& A,vector<int>& B,vector<vector<int>>& mem,int ia,int ib)
+{
+    int& ret=mem[ia+1][ib+1];
+    if(ret>-1)return ret;
+    ret=2;
+    long a=(ia==-1?NEGI:A[ia]);
+    long b=(ib==-1?NEGI:B[ib]);
+    long maxE=max(a,b);
+    for(int i=ia+1;i<n;i++)
+        if(maxE<A[i])
+            ret=max(ret,find_JLIS(A,B,mem,i,ib)+1);
+    for(int i=ib+1;i<m;i++)
+        if(maxE<B[i])
+            ret=max(ret,find_JLIS(A,B,mem,ia,i)+1);
+    return ret;
+}
+
+int main()
+{
+    int C=0;
+    cin>>C;
+    for (int i=0;i<C;i++)
+    {
+        cin>>n>>m;
+        vector<int> a(n,0),b(m,0);
+        vector<vector<int>> mem(n+1,vector<int>(m+1,-1));
+        for(int j=0;j<n;j++) cin>>a[j];
+        for(int j=0;j<m;j++) cin>>b[j];
+        cout<<find_JLIS(a,b,mem,-1,-1)-2<<endl;
+    }
+    return 0;
+}
