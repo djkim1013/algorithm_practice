28ff3ca2eeb5f383165107081ef6202666a00ea8
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 15:38:44 +0900

    BOJ 2292: judge wrong

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 //BOJ: 2292
 //2021.07.14
 //category: 기본수학
-//review: 
+//review:
+//      - 1,1+6,1+6+12,...순으로 층이 멀어지며 같은 층은 최소 층수만큼의 방을 거치면 갈 수 있다.
+//      - 처음과 끝이 포함되므로 마지막에 +1되어야 정답
 
 import java.util.*;
 
@@ -9,7 +11,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int answer = 0;
+        int answer = 1;
+        int floor = 1;
+        while((floor+=answer++*6)<n);
         System.out.println(answer);
         sc.close();
     }
