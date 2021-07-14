54fddeec53b953142f764045668b5b31072ed15b
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 14:45:40 +0900

    BOJ 2941: 1 bigger than answer

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 //BOJ: 2941
 //2021.07.14
 //category: 문자열
-//review: 
+//review: for문 흐름 주의
 
 import java.util.*;
 
@@ -10,21 +10,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         char[] str = sc.next().toCharArray();
         int answer = 0;
-        for(int i=0;i<str.length-1;i++){
-            if(str[i]=='c'){
-                if(str[i+1]=='='||str[i+1]=='-'){
-                    i++;
-                }
-            }else if(i<str.length-2&&str[i]=='d'){
-                if(str[i+1]=='z'&&str[i+2]=='='){
+        for(int i=0;i<str.length;i++){
+            if(i<str.length-2){
+                if(str[i]=='d'&&str[i+1]=='z'&&str[i+2]=='='){
                     i+=2;
                 }
-            }else if(str[i]=='l'||str[i]=='n'){
-                if(str[i+1]=='j'){
+            }else if(i<str.length-2){
+                if(str[i]=='c'&&(str[i+1]=='='||str[i+1]=='-')){
                     i++;
-                }
-            }else if(str[i]=='z'){
-                if(str[i]=='='){
+                }else if((str[i]=='l'||str[i]=='n')&&str[i+1]=='j'){
+                    i++;
+                }else if(str[i]=='z'&&str[i+1]=='='){
                     i++;
                 }
             }
