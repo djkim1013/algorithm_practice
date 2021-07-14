f7c46bbcc05bcaf0ebc51af8322561f35b464ea6
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 11:16:21 +0900

    try add default main codes

---
+++ Main.java
@@ -0,0 +1,12 @@
+//BOJ:
+//2021..
+//문제 분류:
+//해결 분석:
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
