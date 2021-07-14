cdaca3f7a7e597153c3d34131b5e31ff43ad4ab7
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 16:46:44 +0900

    BOJ 2869: time over (0.15sec)

--- Main.java
+++ Main.java
@@ -8,7 +8,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int v = sc.nextInt();
+        int h = 0;
+        int answer = 1;
+        while((h+=a)<v){
+            answer++;
+            h-=b;
+        }
+        System.out.println(answer);
         sc.close();
     }
 }
