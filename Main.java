b11afa1bf123e6ddd5c830feb31c6652c5d5f4e5
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 15:24:27 +0900

    BOJ 2292: start

--- Main.java
+++ Main.java
@@ -1,29 +1,15 @@
-//BOJ: 1712
+//BOJ: 2292
 //2021.07.14
 //category: 기본수학
-//review: 예외처리 항상 주의
+//review: 
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        if(c-b<=0){
-            System.out.println(-1);
-            sc.close();
-            return;
-        }
-        
-        int sum = -a;
+        int n = sc.nextInt();
         int answer = 0;
-        while(sum<=0){
-            sum+=c-b;
-            answer++;
-        }
         System.out.println(answer);
         sc.close();
     }
