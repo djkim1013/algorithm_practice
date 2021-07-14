e1e76483c87a8ff9e9ad575e5346f529d60d35d7
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 15:01:56 +0900

    BOJ 2941: judge wrong2

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 //BOJ: 2941
 //2021.07.14
 //category: 문자열
-//review: for문 흐름 주의, 복붙 오타 주의
-//          if-else if 문을 연달아 쓸때 조건문 흐름 확인
+//review: for문 흐름 주의
+//      복붙 오타 주의
+//      if-else if 문을 연달아 쓸때 조건문 흐름 확인
+//      조건문 누락
 
 import java.util.*;
 
@@ -12,18 +14,18 @@ public class Main {
         char[] str = sc.next().toCharArray();
         int answer = 0;
         for(int i=0;i<str.length;i++){
+            if(i+2<str.length){
+                if(str[i]=='d'&&str[i+1]=='z'&&str[i+2]=='='){
+                    i+=2;
+                }
+            }
             if(i+1<str.length){
                 if(str[i]=='c'&&(str[i+1]=='='||str[i+1]=='-')){
                     i++;
                 }else if((str[i]=='l'||str[i]=='n')&&str[i+1]=='j'){
                     i++;
-                }else if(str[i]=='z'&&str[i+1]=='='){
+                }else if((str[i]=='z'||str[i]=='s')&&str[i+1]=='='){
                     i++;
-                }
-            }
-            if(i+2<str.length){
-                if(str[i]=='d'&&str[i+1]=='z'&&str[i+2]=='='){
-                    i+=2;
                 }
             }
             answer++;
