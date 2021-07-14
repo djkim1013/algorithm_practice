9bf6ae9df3a9c1f34fe977fe13e9b5b35dc1458c
djkim1013 <dongjukim1013@gmail.com>
2021-07-14 11:31:38 +0900

    add testfiles

---
+++ Main.java
@@ -0,0 +1,14 @@
+//BOJ: 
+//2021.07.14
+//문제 분류:
+//해결 분석:
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
