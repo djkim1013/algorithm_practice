4eb796918829dafc78d86fb13c0f597e74c03810
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 13:50:58 +0900

    BOJ 10757: throw work

--- Main.java
+++ Main.java
@@ -2,11 +2,16 @@
 //2021.07.16
 //category: 기본수학
 //review:
+//      - char[]: 코드가 너무 복잡해져서 지움
 
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = in.readLine();
+        // out.write(answer.reverse().toString());
+        out.flush();
     }
 }
