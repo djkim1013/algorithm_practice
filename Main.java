5a168ab1eb4b9b89313c0fca0eda1b443a4c6fab
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 14:37:27 +0900

    BOJ 1011: start

--- Main.java
+++ Main.java
@@ -1,35 +1,23 @@
-//BOJ: 10757
+//BOJ: 1011
 //2021.07.16
 //category: 기본수학
 //review:
-//      - 코드가 너무 복잡해져서 지움
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = in.readLine();
-        StringBuilder a = new StringBuilder(input.substring(0,input.indexOf(" ")));
-        StringBuilder b = new StringBuilder(input.substring(input.indexOf(" ")+1));
-        a.reverse();
-        b.reverse();
-        StringBuilder answer = new StringBuilder();
-        for(int i=0;i<Math.max(a.length(),b.length());i++){
-            int sum = 0;
-            if(i<a.length()) sum+=Character.getNumericValue(a.charAt(i));
-            if(i<b.length()) sum+=Character.getNumericValue(b.charAt(i));
-            if(i<answer.length()) sum+=Character.getNumericValue(answer.charAt(i));
-            else answer.append('0');
-            if(sum>=10){
-                answer.append((char)(sum/10+'0'));
-                sum%=10;
-            }
-            answer.setCharAt(i,(char)(sum+'0'));
+        int t = Integer.parseInt(in.readLine());
+        for(int test=0;test<t;test++){
+            StringTokenizer st=new StringTokenizer(in.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int answser = 0;
+            out.write(Integer.toString(answer));
         }
-        answer.reverse();
-        out.write(answer.toString());
         out.flush();
     }
 }
