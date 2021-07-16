11c9cc016cc9214db6c6abc4cf4f7e83539e24c5
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 10:10:43 +0900

    BOJ 2869: judge wrong

--- Main.java
+++ Main.java
@@ -5,22 +5,25 @@
 //      - while문으로 작성 시 시간초과
 //      - 나눗셈으로 작성 시 시간초과
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int v = sc.nextInt();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = in.readLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
         int answer = v-a;
         if(answer<=0){
             System.out.println(1);
-            sc.close();
             return;
         } 
-        answer = answer/(a-b) + 1;
-        System.out.println(++answer);
-        sc.close();
+        answer /= a-b;
+        out.write(Integer.toString(++answer));
+        out.flush();
     }
 }
