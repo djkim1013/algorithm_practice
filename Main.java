557b7c6b0e79bb6323e0a0bbc51ae4cb5733c12b
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 11:42:08 +0900

    BOJ 2775: judged wrong

--- Main.java
+++ Main.java
@@ -1,29 +1,34 @@
-//BOJ: 10250
+//BOJ: 2775
 //2021.07.16
 //category: 기본수학
 //review:
-//      - 오답: 경계값 처리 실패
-//          나눗셈에서 나머지가 0이 되는 순간 확인할 것
+//      - 오류: k,n 범위 오류 방지를 위해 배열을 넉넉하게 잡는다.
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[][] apt = new int[15][15];
+    static int countNeighbor(int k, int n){
+        if(k==0) return n;
+        int ret = apt[k][n];
+        if(ret>-1) return ret;
+        ret = 0;
+        for(int i=1;i<=n;i++){
+            ret+=countNeighbor(k-1,i);
+        }
+        apt[k][n] = ret;
+        return ret;
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(in.readLine());
+        for(int i=0;i<14;i++)Arrays.fill(apt[i],-1);
         for(int test=0;test<t;test++){
-            String input = in.readLine();
-            StringTokenizer st = new StringTokenizer(input," ");
-            int h = Integer.parseInt(st.nextToken());
-            // int w = Integer.parseInt(st.nextToken());
-            new String(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
-            int answer=0;
-            answer = (n%h)*100;
-            if (answer == 0) answer = h*100;
-            answer += (n-1)/h+1;
+            int k = Integer.parseInt(in.readLine());
+            int n = Integer.parseInt(in.readLine());
+            int answer=countNeighbor(k, n);
             out.write(answer+"\n");
         }
         out.flush();
