5c47ac648f3bd254758c843615825f30725149b1
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 10:32:44 +0900

    BOJ 10250: judge wrong

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 //2021.07.16
 //category: 기본수학
 //review:
+//      - 오답: w를 고려하지 않았음
 
 import java.io.*;
 import java.util.*;
@@ -10,10 +11,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = in.readLine();
-        StringTokenizer st = new StringTokenizer(input," ");
-        int answer=0;
-        out.write(Integer.toString(answer));
+        int t = Integer.parseInt(in.readLine());
+        for(int test=0;test<t;test++){
+            String input = in.readLine();
+            StringTokenizer st = new StringTokenizer(input," ");
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int answer=0;
+            answer = (n%h)*100;
+            answer += n/h+1;
+
+            out.write(answer+"\n");
+        }
         out.flush();
     }
 }
