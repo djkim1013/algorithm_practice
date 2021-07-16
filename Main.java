9c89373542c3d66b3c7527e125821704de965ac6
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 10:23:20 +0900

    BOJ 10250: star, BOJ 2869: fix date

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-//BOJ: 2869
-//2021.07.15
+//BOJ: 10250
+//2021.07.16
 //category: 기본수학
 //review:
-//      - while문으로 작성 시 시간초과
-//      - 나눗셈으로 작성 시 시간초과
-//      - 입출력을 버퍼스트림으로 변경: 오답
-//      - 나눗셈의 나머지가 0보다 큰 경우 +1
 
 import java.io.*;
 import java.util.*;
@@ -16,17 +12,8 @@ public class Main {
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         String input = in.readLine();
         StringTokenizer st = new StringTokenizer(input," ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-        int answer = v-a;
-        if(answer<=0){
-            System.out.println(1);
-            return;
-        } 
-        answer /= a-b;
-        if((v-a)%(a-b)>0)answer++;
-        out.write(Integer.toString(++answer));
+        int answer=0;
+        out.write(Integer.toString(answer));
         out.flush();
     }
 }
