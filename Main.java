555fb08ae46b6c0db808ce7ab040f7f2fa0443d8
djkim1013 <dongjukim1013@gmail.com>
2021-07-16 16:50:49 +0900

    BOJ 1011: runtime error3

--- Main.java
+++ Main.java
@@ -3,36 +3,38 @@
 //category: 기본수학
 //review:
 //      - x<y<2^31: stack overflow
-//      - k stack overflow
+//      - 무한루프: 도착 좌표가 아니라 이동 거리로 체크
+//      - 시간초과: 완전탐색 최적화
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int goal;
-    static int calDays(int cur,int k){
-        if(cur>=goal){
-            if(cur==goal&&k==1)return 0;
+    static int calDays(int dist,int k){
+        if(dist<=0){
+            if(dist==0&&k==1)return 0;
             return 0xfff0000;
         }
-        int ret=0xfff0000;
+        int ret=0;
         for(int i=k-1;i<=k+1;i++){
-            if(i<=0||i>=Integer.MAX_VALUE-1)continue;
-            if(cur>=Integer.MAX_VALUE-i)continue;
-            ret=Math.min(calDays(cur+i,i),ret);
+            if(i<=0)continue;
+            if(ret==0){
+                ret=calDays(dist-i,i);
+                continue;
+            } 
+            ret=Math.min(calDays(dist-i,i),ret);
         }
-        return ++ret;
+        return ret<Integer.MAX_VALUE?++ret:ret;
     }
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        out.write(((1<<31)-1)+" "+Integer.MAX_VALUE);
         int t = Integer.parseInt(in.readLine());
         for(int test=0;test<t;test++){
             StringTokenizer st=new StringTokenizer(in.readLine()," ");
             int cur = Integer.parseInt(st.nextToken());
-            goal = Integer.parseInt(st.nextToken());
-            int answer = calDays(cur,0);
+            int dist = Integer.parseInt(st.nextToken())-cur;
+            int answer = calDays(dist,0);
             out.write(Integer.toString(answer)+"\n");
         }
         out.flush();
