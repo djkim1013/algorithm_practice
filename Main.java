9321e8afe2b3970c4859b55dee71b0f7ae52d2bd
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-07-18 03:45:43 +0900

    reset Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,12 @@
-//BOJ: 4948
-//2021.07.18
-//category: 기본수학
+//BOJ: 
+//2021.07.
+//category: 
 //review:
 
 import java.io.*;
 // import java.util.*;
 
 public class Main {
-    static boolean judgePrime(int n){
-        if(n==1)return false;
-        for(int i=2;i<=(int)Math.sqrt(n);i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    }
-
     public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n=0;
-        while((n=Integer.parseInt(in.readLine()))!=0){
-            int answer=0;
-            for(int i=n+1;i<=2*n;i++){
-                if(judgePrime(i))answer++;
-            }
-            out.write(answer+"\n");
-        }
-        out.flush();
     }
 }
