557b41c5e76d3abfdcc8075f39abc1940b610003
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-07-18 03:11:25 +0900

    BOJ 2581: judged wrong

--- Main.java
+++ Main.java
@@ -4,13 +4,34 @@
 //review:
 
 import java.io.*;
-import java.util.*;
+// import java.util.*;
 
 public class Main {
-        public static void main(String[] args) throws Exception {
+    static int judgePrime(int n){
+        if(n==1)return 0;
+        for(int i=2;i<(int)Math.sqrt(n);i++){
+            if(n%i==0)return 0;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+        int m=Integer.parseInt(in.readLine());
+        int n=Integer.parseInt(in.readLine());
+        int sum=0;
+        int min=0xf0000;
+        for(int i=m;i<=n;i++){
+            int number=judgePrime(i);
+            sum+=number;
+            if(number>0)min=Math.min(min,number);
+        }
+        if(min==0xf0000){
+            out.write("-1");
+        }else{
+            out.write(sum+"\n"+min);
+        }
         out.flush();
     }
 }
