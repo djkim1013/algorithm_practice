67362824a879b0ac6fda35a37db7f1e655adcd93
djkim1013 <dongjukim1013@gmail.com>
2021-07-19 11:57:01 +0900

    BOJ 10872: start

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-//BOJ: 1002
+//BOJ: 10872
 //2021.07.19
-//category: 기본수학
+//category: 재귀함수 구현
 //review:
 
 import java.io.*;
@@ -11,36 +11,8 @@ public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        int test = Integer.parseInt(in.readLine());
-        for(int t=0;t<test;t++){
-            int[] a=new int[3];
-            int[] b=new int[3];
-            StringTokenizer st = new StringTokenizer(in.readLine()," ");
-            for(int i=0;i<3;i++){
-                a[i]=Integer.parseInt(st.nextToken());
-            }
-            for(int i=0;i<3;i++){
-                b[i]=Integer.parseInt(st.nextToken());
-            }
-            if(a[0]==b[0]&&a[1]==b[1]){
-                if(a[2]==b[2]) out.write("-1\n");
-                else out.write("0\n");
-                continue;
-            }
-            int dist = (a[0]-b[0])*(a[0]-b[0]);
-            dist += (a[1]-b[1])*(a[1]-b[1]);
-            if((a[2]+b[2])*(a[2]+b[2])<dist||
-                (a[2]-b[2])*(a[2]-b[2])>dist){
-                out.write("0\n");
-                continue;
-            }
-            if((a[2]+b[2])*(a[2]+b[2])==dist||
-                (a[2]-b[2])*(a[2]-b[2])==dist){
-                out.write("1\n");
-                continue;
-            }
-            out.write("2\n");
-        }
+        int n = Integer.parseInt(in.readLine());
+        
         out.flush();
     }
 }
