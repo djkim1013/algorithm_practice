1ab8756b588965635432608d9a949929eca000fa
djkim1013 <dongjukim1013@gmail.com>
2021-07-19 12:05:55 +0900

    reset main

--- Main.java
+++ Main.java
@@ -7,15 +7,10 @@ import java.io.*;
 // import java.util.*;
 
 public class Main {
-    static int calFibonacci(int n){
-        if(n<2)return n;
-        return calFibonacci(n-1)+calFibonacci(n-2);
-    }
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(in.readLine());
-        out.write(calFibonacci(n)+"\n");
         out.flush();
     }
 }
