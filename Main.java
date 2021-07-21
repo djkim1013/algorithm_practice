e3587b1eeb1a11494185d82ea0c3ec2825822d15
djkim1013 <dongjukim1013@gmail.com>
2021-07-21 13:57:39 +0900

    BOJ 2108: example wrong

--- Main.java
+++ Main.java
@@ -2,14 +2,38 @@
 //2021.07.21
 //category: 정렬
 //review:
+//      -반올림 함수:Math.round()
+//      -mode 계산법 실수
 
 import java.io.*;
-// import java.util.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(in.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(in.readLine());
+        }
+        Arrays.sort(arr);
+        int answer=0;
+        //mean
+        for(int i:arr){
+            answer+=i;
+        }
+        answer=(int)Math.round((double)answer/n);
+        out.write(answer+"\n");
+        //middle value
+        answer=arr[n/2];
+        out.write(answer+"\n");
+        //mode
+        
+        out.write(answer+"\n");
+        //range
+        answer=arr[n-1]-arr[0];
+        out.write(answer+"\n");
         out.flush();
     }
 }
