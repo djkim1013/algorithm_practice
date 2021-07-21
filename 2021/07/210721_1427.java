762cf241bef74da7b28bb4196456caa095ade7e7
djkim1013 <dongjukim1013@gmail.com>
2021-07-21 15:58:03 +0900

    BOJ 1427: update

--- 2021/07/210721_1427.java
+++ 2021/07/210721_1427.java
@@ -10,12 +10,15 @@ public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] str = in.readLine().toCharArray();
-        Arrays.sort(str);
-        for(int i=str.length-1;i>=0;i--){
-            out.write(str[i]);
+        String input = in.readLine();
+        Character[] arr = new Character[input.length()];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.charAt(i);
         }
-        out.write(str.toString());
+        Arrays.sort(arr,Collections.reverseOrder());
+        for(Character c:arr){
+            out.write(c);
+        }
         out.flush();
     }
 }
--- Main.java
+++ Main.java
@@ -10,6 +10,15 @@ public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = in.readLine();
+        Character[] arr = new Character[input.length()];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.charAt(i);
+        }
+        Arrays.sort(arr,Collections.reverseOrder());
+        for(Character c:arr){
+            out.write(c);
+        }
         out.flush();
     }
 }
