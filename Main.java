996ba425744a87444f2cb16e22d89c9f948f3c63
djkim1013 <dongjukim1013@gmail.com>
2021-07-22 09:50:41 +0900

    BOJ 18870: memory over

--- Main.java
+++ Main.java
@@ -1,45 +1,37 @@
-//BOJ: 10814
+//BOJ: 18870
 //2021.07.21
 //category: 정렬
 //review:
-//      - 문제 오독: 나이가 같으면 이름순 정렬이 아니라 가입일 순 정렬이다.
+//      - 메모리 초과: 2*10^9 크기의 배열은 너무 크다.
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static public class User implements Comparable<User>{
-        Integer index;
-        Integer age;
-        String name;
-        public User(int index,int age,String name){
-            this.index=index;
-            this.age=age;
-            this.name=name;
-        }
-        public int compareTo(User other){
-            if(age.compareTo(other.age)==0) return index.compareTo(other.index);
-            return age.compareTo(other.age);
-
-        }
-    }
 
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(in.readLine());
-        User[] arr = new User[n];
+        StringTokenizer st = new StringTokenizer(in.readLine()," ");
+        int[] arr = new int[n];
         for(int i=0;i<n;i++){
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            arr[i]=new User(i,age,name);
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        for(int i=0;i<n;i++){
-            out.write(arr[i].age+" "+arr[i].name);
-            out.newLine();
+        int[] sortArr=arr.clone();
+        Arrays.sort(sortArr);
+        int range=sortArr[n-1]-sortArr[0]+1;
+        int[] count=new int[range];
+        int min=sortArr[0];
+        for(int i:arr){
+            if(count[i-min]==0) count[i-min]++;
+        }
+        count[0]--;
+        for(int i=1;i<range;i++){
+            count[i]+=count[i-1];
+        }
+        for(int i:arr){
+            out.write(count[i-min]+" ");
         }
         out.flush();
     }
