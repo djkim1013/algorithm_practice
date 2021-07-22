de859d5f2995224bbe249e1f476e4441f9582c3f
djkim1013 <dongjukim1013@gmail.com>
2021-07-22 10:35:37 +0900

    BOJ 18870: time over

--- Main.java
+++ Main.java
@@ -3,12 +3,21 @@
 //category: 정렬
 //review:
 //      - 메모리 초과: 2*10^9 크기의 배열은 너무 크다.
-//      - 시간 초과
+//      - 시간 초과: ArrayList로 중복값 없는 리스트를 선언하고 정렬했지만 시간초과. collection 정렬이 느린것으로 보임
+//      - 시간 초과:
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int findIndex(int value,int[] arrSort){
+        int index=0;
+        for(int i=0;i+1<arrSort.length;i++){
+            if(value<=arrSort[i]) return index;
+            if(arrSort[i]<arrSort[i+1]) index++;
+        }
+        return index;
+    }
 
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -16,14 +25,13 @@ public class Main {
         int n = Integer.parseInt(in.readLine());
         StringTokenizer st = new StringTokenizer(in.readLine()," ");
         int[] arr = new int[n];
-        ArrayList<Integer> arrSort = new ArrayList<Integer>();
         for(int i=0;i<n;i++){
             arr[i]=Integer.parseInt(st.nextToken());
-            if(!arrSort.contains(arr[i])) arrSort.add(arr[i]);
         }
-        Collections.sort(arrSort);
+        int[] arrSort = arr.clone();
+        Arrays.sort(arrSort);
         for(int i:arr){
-            out.write(arrSort.indexOf(i)+" ");
+            out.write(findIndex(i,arrSort)+" ");
         }
         out.flush();
     }
