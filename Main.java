47cdb1683583876b2dc92e4f8cb15eba12dd679e
djkim1013 <dongjukim1013@gmail.com>
2021-07-22 10:21:43 +0900

    BOJ 18870: time over

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 //BOJ: 18870
-//2021.07.21
+//2021.07.22
 //category: 정렬
 //review:
 //      - 메모리 초과: 2*10^9 크기의 배열은 너무 크다.
+//      - 시간 초과
 
 import java.io.*;
 import java.util.*;
@@ -15,23 +16,14 @@ public class Main {
         int n = Integer.parseInt(in.readLine());
         StringTokenizer st = new StringTokenizer(in.readLine()," ");
         int[] arr = new int[n];
+        ArrayList<Integer> arrSort = new ArrayList<Integer>();
         for(int i=0;i<n;i++){
             arr[i]=Integer.parseInt(st.nextToken());
+            if(!arrSort.contains(arr[i])) arrSort.add(arr[i]);
         }
-        int[] sortArr=arr.clone();
-        Arrays.sort(sortArr);
-        int range=sortArr[n-1]-sortArr[0]+1;
-        int[] count=new int[range];
-        int min=sortArr[0];
+        Collections.sort(arrSort);
         for(int i:arr){
-            if(count[i-min]==0) count[i-min]++;
-        }
-        count[0]--;
-        for(int i=1;i<range;i++){
-            count[i]+=count[i-1];
-        }
-        for(int i:arr){
-            out.write(count[i-min]+" ");
+            out.write(arrSort.indexOf(i)+" ");
         }
         out.flush();
     }
