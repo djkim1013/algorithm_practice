570901ff662d6a195109167d80431b7175e347ae
djkim1013 <dongjukim1013@gmail.com>
2021-07-23 12:21:34 +0900

    BOJ 9663: save

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-//BOJ: 15652
+//BOJ: 9663
 //2021.07.23
 //category: backtracking
 //review:
@@ -7,25 +7,31 @@
 import java.io.*;
 
 public class Main{
-    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m;
-    static void list(int idx,String output)throws IOException{
-        if(output.length()==m){
-            for(int i=0;i<m;i++){
-                out.write(output.charAt(i)+" ");
+    static int n;
+    static int nQueen(int row,int col,int count,boolean[][] board){
+        if(count==n){
+           return 1; 
+        }
+        if(row==n){
+            return 0;
+        }
+        if(col==n){
+            return nQueen(row+1,0,count,board);
+        }
+        int ret = 0;
+        ret += nQueen(row,col+1,count,board);
+        if(!board[row][col]){
+            for(int i=0;i<n;i++){
+                if(!board[row][i]) board[row][i]=true;
+                if(!board[i][col]) board[i][col]=true;
             }
-            out.newLine();
-            return;
+            for(int i=)
         }
-        for(int i=idx;i<=n;i++){
-            list(i,output+i);
-        }
+        return ret;
     }
     public static void main(String[] args)throws IOException{
-        n = System.in.read()-'0';
-        System.in.read();
-        m = System.in.read()-'0';
-        list(1,"");
-        out.flush();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(in.readLine());
+        System.out.print(nQueen(0,0,0,new boolean[n][n]));
     }
 }
