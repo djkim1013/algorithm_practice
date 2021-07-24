4d1236bf1b832c54d7ae9b089dd58b25db8c716f
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-07-24 23:54:45 +0900

    BOJ 9663: memory over4

--- Main.java
+++ Main.java
@@ -3,35 +3,31 @@
 //category: backtracking
 //review:
 //      - 다차원 배열은 clone()으로는 깊은 복사가 되지 않음 주의
-//      - 메모리 초과: 당연한 경우의 수 체크가 많음
+//      - 메모리 초과: 재귀함수에서 필요없는 계산 제거
 
 // import java.util.*;
 import java.io.*;
 
 public class Main{
     static int n;
-    int nQueen(int row,int col,int count,int[] board){
-        if(count==n){
-            return 1; 
+    boolean boardCheck(int row, int idx, int[] board){
+        for(int i=0;i<row;i++){
+            if(board[i]==idx) return false;
+            if(board[i]+row-i==idx) return false;
+            if(board[i]-row+i==idx) return false;
         }
+        return true;
+    }
+    int nQueen(int row,int[] board){
         if(row==n){
-            return 0;
-        }
-        if(col==n){
-            return nQueen(row+1,0,count,board);
+            return 1;
         }
         int ret = 0;
-        ret += nQueen(row,col+1,count,board);
-        if((board[row]&(1<<col))==0){
+        for(int i=0;i<n;i++){
+            if(!boardCheck(row,i,board))continue;
             int[] boardNew = board.clone();
-            for(int i=0;i<n;i++){
-                if((boardNew[row]&(1<<i))==0) boardNew[row]+=1<<i;
-                if((boardNew[i]&(1<<col))==0) boardNew[i]+=1<<col;
-                
-                if(row-i>=0&&col-i>=0&&(boardNew[row-i]&(1<<(col-i)))==0) boardNew[row-i]+=1<<(col-i);
-                if(row-i>=0&&col+i<n&&(boardNew[row-i]&(1<<(col+i)))==0) boardNew[row-i]+=1<<(col+i);
-            }
-            ret+=nQueen(row+1,0,count+1,boardNew);
+            boardNew[row]=i;
+            ret+=nQueen(row+1,boardNew);
         }
         return ret;
     }
@@ -39,6 +35,6 @@ public class Main{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(in.readLine());
         Main m = new Main();
-        System.out.print(m.nQueen(0,0,0,new int[n]));
+        System.out.print(m.nQueen(0,new int[n]));
     }
 }
