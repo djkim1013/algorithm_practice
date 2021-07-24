8edbdbc2ab2e088a32cc01d34a5d6096a226962b
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-07-24 23:23:43 +0900

    BOJ 9663: memory over3

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@
 //category: backtracking
 //review:
 //      - 다차원 배열은 clone()으로는 깊은 복사가 되지 않음 주의
-//      - 메모리 초과:
+//      - 메모리 초과: 당연한 경우의 수 체크가 많음
 
 // import java.util.*;
 import java.io.*;
 
 public class Main{
     static int n;
-    static int nQueen(int row,int col,int count,int[] board){
+    int nQueen(int row,int col,int count,int[] board){
         if(count==n){
             return 1; 
         }
@@ -30,16 +30,15 @@ public class Main{
                 
                 if(row-i>=0&&col-i>=0&&(boardNew[row-i]&(1<<(col-i)))==0) boardNew[row-i]+=1<<(col-i);
                 if(row-i>=0&&col+i<n&&(boardNew[row-i]&(1<<(col+i)))==0) boardNew[row-i]+=1<<(col+i);
-                if(row+i<n&&col-i>=0&&(boardNew[row+i]&(1<<(col-i)))==0) boardNew[row+i]+=1<<(col-i);
-                if(row+i<n&&col+i<n&&(boardNew[row+i]&(1<<(col+i)))==0) boardNew[row+i]+=1<<(col+i);
             }
-            ret+=nQueen(row,col+1,count+1,boardNew);
+            ret+=nQueen(row+1,0,count+1,boardNew);
         }
         return ret;
     }
     public static void main(String[] args)throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(in.readLine());
-        System.out.print(nQueen(0,0,0,new int[n]));
+        Main m = new Main();
+        System.out.print(m.nQueen(0,0,0,new int[n]));
     }
 }
