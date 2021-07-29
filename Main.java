da023dbe989256b1467324574414f83a20e8a19c
djkim1013 <dongjukim1013@gmail.com>
2021-07-29 14:22:57 +0900

    BOJ 9461: wrong

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-//BOJ: 1904
+//BOJ: 9461
 //2021.07.29
 //category: dynamic programming
 //review:
@@ -7,21 +7,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static final int MOD = 15746;
-    static int[] cache = new int[1000001];
-    static int makeNum(int n){
-        if(n<=2) return n;
+    static int[] cache = new int[101];
+    static int padovan(int n){
+        if(n<=0) return 0;
+        if(n<=2) return 1;
         int ret = cache[n];
         if(ret!=-1) return ret;
-        ret = makeNum(n-2);
-        ret += makeNum(n-1);
-        cache[n] = ret %= MOD;
+        ret = padovan(n-3);
+        ret += padovan(n-2);
+        cache[n] = ret;
         return ret;
     }
     public static void main(String[] args)throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(in.readLine());
+        StringBuilder out = new StringBuilder();
+        int test = Integer.parseInt(in.readLine());
         Arrays.fill(cache,-1);
-        System.out.print(makeNum(n));
+        for(int t=0;t<test;t++){
+            int n = Integer.parseInt(in.readLine());
+            out.append(padovan(n)+"\n");
+        }
+        System.out.print(out);
     }
 }
