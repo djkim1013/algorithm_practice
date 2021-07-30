7e52d7f9561a8719433c8294e4218610d9c656b0
djkim1013 <dongjukim1013@gmail.com>
2021-07-30 11:53:42 +0900

    BOJ 1463: wrong

--- Main.java
+++ Main.java
@@ -10,17 +10,16 @@ public class Main{
     static int[] cache;
     static int n;
     static int makeN(int i){
-        if(i==n) return 0;
+        if(i==1) return 0;
         int ret = cache[i];
         if(ret>-1) return ret;
         ret=1000000;
-        if(i*3<=n) ret=Math.min(makeN(i*3)+1,ret);
-        if(i*2<=n) ret=Math.min(makeN(i*2)+1,ret);
-        if(i*2>n&&i*3>n){
-            System.out.println(i);
-            ret=Math.min(n-i,ret);
+        if(i%3==0) ret=Math.min(makeN(i/3),ret);
+        if(i%2==0) ret=Math.min(makeN(i/2),ret);
+        if(i%2>0&&i%3>0){
+            ret=Math.min(makeN(i-1),ret);
         }
-        cache[i]=ret;
+        cache[i]=++ret;
         return ret;
     }
     public static void main(String[] args)throws IOException{
@@ -28,6 +27,6 @@ public class Main{
         n = Integer.parseInt(in.readLine());
         cache=new int[n+1];
         Arrays.fill(cache,-1);
-        System.out.println(makeN(1));
+        System.out.println(makeN(n));
     }
 }
