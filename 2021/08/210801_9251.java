c9ee1c68d13dfb6e7a496b634531e28183260265
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-08-01 22:38:12 +0900

    BOJ 9251: optimization

--- 2021/08/210801_9251.java
+++ 2021/08/210801_9251.java
@@ -1,4 +1,3 @@
-
 //BOJ: 9251
 //2021.08.01
 //category: dynamic programming
@@ -8,48 +7,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static String strA;
-    static String strB;
+    static char[] strA;
+    static char[] strB;
     static int lenA, lenB;
     static int[][] cache;
 
-    static int lcs(int i1, int i2){
-        if(i1==-1){
-            int ret = 0;
-            for(int i=0;i<lenA;i++){
-                int j=i2;
-                while(++j<lenB){
-                    if(strA.charAt(i)==strB.charAt(j)){
-                        ret = Math.max(ret, lcs(i,j));
-                        break;
-                    }
-                }
-            }
-            return ret;
-        }
-        int ret = cache[i1][i2];
+    static int lcs(int idxA, int idxB){
+        if(idxA==lenA||idxB==lenB) return 0;
+        int ret = cache[idxA][idxB];
         if(ret>-1) return ret;
         ret = 0;
-        for(int i=i1+1;i<lenA;i++){
-            int j=i2;
-            while(++j<lenB){
-                if(strA.charAt(i)==strB.charAt(j)){
-                    ret = Math.max(ret, lcs(i,j));
-                    break;
-                }
+        for(int i=idxB;i<lenB;i++){
+            if(strA[idxA]==strB[i]){
+                ret = Math.max(ret, lcs(idxA+1,i+1)+1);
+                break;
             }
         }
-        return cache[i1][i2] = ++ret;
+        ret = Math.max(ret, lcs(idxA+1,idxB));
+        return cache[idxA][idxB] = ret;
     }
 
     public static void main(String[] args)throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        strA = in.readLine();
-        lenA = strA.length();
-        strB = in.readLine();
-        lenB = strB.length();
+        strA = in.readLine().toCharArray();
+        lenA = strA.length;
+        strB = in.readLine().toCharArray();
+        lenB = strB.length;
         cache = new int[lenA][lenB];
         for(int i=0;i<lenA;i++) Arrays.fill(cache[i],-1);
-        System.out.print(lcs(-1,-1));
+        System.out.print(lcs(0,0));
     }
 }
