b9ab361728f7e78d4baaaf34ea95b02e10792313
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-08-01 19:42:21 +0900

    BOJ 9251: time over

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,5 +1,5 @@
 
-//BOJ: 2565
+//BOJ: 9251
 //2021.08.01
 //category: dynamic programming
 //review:
@@ -8,74 +8,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int[][] lines;
-    static int[] cache;
-    static int n;
+    static String strA;
+    static String strB;
+    static int lenA, lenB;
+    static int[][] cache;
 
-    static void mergeSort(int start,int end){
-        for(int i=start+1;i<=end;i++){
-            int cur=i;
-            while(--cur>=start){
-                if(lines[cur][0]<lines[i][0]){
-                    break;
-                }
-            }
-            if(++cur<i){
-                int swap1 = lines[i][0];
-                int swap2 = lines[i][1];
-                for(int j=i;j>cur;j--){
-                    lines[j][0] = lines[j-1][0];
-                    lines[j][1] = lines[j-1][1];
-                }
-                lines[cur][0] = swap1;
-                lines[cur][1] = swap2;
-            }
-        }
-    }
-    static void sortLines(int start, int end){
-        if(start==end) return;
-        int mid = start+end;
-        mid /= 2;
-        sortLines(start,mid);
-        sortLines(mid+1,end);
-        mergeSort(start,end);
-    }
-
-    static int maxUncrossed(int idx){
-        if (idx==n){
-            return 0;
-        }
-        if(idx==-1){
+    static int lcs(int i1, int i2){
+        if(i1==lenA||i2==lenB) return 0;
+        if(i1==-1){
             int ret = 0;
-            for(int i=idx+1;i<n;i++){
-                ret = Math.max(ret, maxUncrossed(i));
+            for(int i=0;i<lenA;i++){
+                int j=i2;
+                while(++j<lenB){
+                    if(strA.charAt(i)==strB.charAt(j)){
+                        ret = Math.max(ret, lcs(i,j));
+                    }
+                }
             }
             return ret;
         }
-        int ret = cache[idx];
+        int ret = cache[i1][i2];
         if(ret>-1) return ret;
         ret = 0;
-        for(int i=idx+1;i<n;i++){
-            if(lines[idx][1]<lines[i][1]){
-                ret = Math.max(ret, maxUncrossed(i));
+        for(int i=i1+1;i<lenA;i++){
+            int j=i2;
+            while(++j<lenB){
+                if(strA.charAt(i)==strB.charAt(j)){
+                    ret = Math.max(ret, lcs(i,j));
+                }
             }
         }
-        cache[idx] = ++ret;
-        return ret;
+        return cache[i1][i2] = ++ret;
     }
 
     public static void main(String[] args)throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(in.readLine());
-        lines = new int[n][2];
-        cache = new int[n];
-        Arrays.fill(cache,-1);
-        for(int i=0;i<n;i++){
-            StringTokenizer input = new StringTokenizer(in.readLine());
-            lines[i][0] = Integer.parseInt(input.nextToken());
-            lines[i][1] = Integer.parseInt(input.nextToken());
-        }
-        sortLines(0,n-1);
-        System.out.print(n-maxUncrossed(-1));
+        strA = in.readLine();
+        lenA = strA.length();
+        strB = in. readLine();
+        lenB = strB.length();
+        cache = new int[lenA][lenB];
+        for(int i=0;i<lenA;i++) Arrays.fill(cache[i],-1);
+        System.out.print(lcs(-1,-1));
     }
 }
