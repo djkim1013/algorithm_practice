dbfdddc5e5eee61f87e752ea3f840faa91c8b97e
djkim1013 <dongjukim1013@gmail.com>
2021-08-03 16:51:41 +0900

    save

--- java_test/Main.java
+++ java_test/Main.java
@@ -9,6 +9,13 @@ import java.io.*;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int goal = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n];
+        for(int i=0;i<n;i++){
+            coins[i] = Integer.parseInt(in.readLine());
+        }
 	}
 }
