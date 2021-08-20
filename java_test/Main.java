605622fce743f2f6a82dc1019ec86af707d5cd61
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-08-21 03:08:48 +0900

    AOJ JOSEPHUS: wrong answer

--- java_test/Main.java
+++ java_test/Main.java
@@ -13,15 +13,15 @@ public class Main {
         for(int t = 0; t < test; t++){
             String input = br.readLine();
             int n = Integer.parseInt(input.substring(0, input.indexOf(" ")));
-            int k = Integer.parseInt(input.substring(input.indexOf(" ")));
+            int k = Integer.parseInt(input.substring(input.indexOf(" ")+1));
             LinkedList<Integer> soldiers = new LinkedList<Integer>();
             for(int i=0;i<n;i++){
                 soldiers.add(i);
             }
-            int i=0;
-            for(;soldiers.size()>2;i=(i+k)%soldiers.size()){
+            for(int i=0;soldiers.size()>2;i=(i+k)%soldiers.size()){
                 soldiers.remove(i);
             }
+            System.out.println(soldiers.get(0)+" "+soldiers.get(1));
         }
 	}
 }
