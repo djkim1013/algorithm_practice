a471215d80dde52033a2ffdc9530e56eeb119a4b
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-08-28 17:25:05 +0900

    AOJ RUNNINGMEDIAN: test wrong

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,125 +1,109 @@
-//AOJ: INSERTION
+//AOJ: RUNNINGMEDIAN
 //2021.08.28
-//category: 이진검색트리
+//category: 우선순위 큐와 힙
 //review:
-//      -treat: tree+heap ~ 비교적 구현이 쉬운 편이니 숙지할것
 
 import java.util.*;
 import java.io.*;
 
-class Node{
-    int key;
-    double priority;
-    int size;
-    Node right, left;
-    public Node(int key){
-        this.key=key;
-        priority=Math.random();
-        size=1;
-    }
-    void setRight(Node right){
-        this.right=right;
-        calSize();
-    }
-    void setLeft(Node left){
-        this.left=left;
-        calSize();
-    }
-    void calSize(){
-        size=1;
-        if(right!=null) size+=right.size;
-        if(left!=null) size+=left.size;
-    }
-}
-
 public class Main {
-    static Node[] split(Node root, int key){
-        if(root==null) return new Node[]{null,null};
-        if(root.key<key){
-            Node[] rs=split(root.right,key);
-            root.setRight(rs[0]);
-            return new Node[]{root,rs[1]};
-        }
-        Node[] ls=split(root.left,key);
-        root.setLeft(ls[1]);
-        return new Node[]{ls[0],root};
+    static int a, b;
+    static long A;
+    static int inputGen(){
+        A=(A*a+b)%20090711;
+        return (int)A;
     }
-    static Node addNode(Node root, Node node){
-        if(root==null) return node;
-        if(root.priority<node.priority){
-            Node[] splitted=split(root,node.key);
-            node.setLeft(splitted[0]);
-            node.setRight(splitted[1]);
-            return node;
-        }else if(root.key>node.key){
-            root.setLeft(addNode(root.left,node));
-        }else{
-            root.setRight(addNode(root.right,node));
-        }
-        return root;
+    static ArrayList<Integer> heap1, heap2;
+    static void swapNode(ArrayList<Integer> heap,int a,int b){
+        int temp = heap.get(a);
+        heap.set(a,heap.get(b));
+        heap.set(b,temp);
     }
-    static Node merge(Node a,Node b){
-        if(a==null) return b;
-        if(b==null) return a;
-        if(a.priority<b.priority){
-            b.setLeft(merge(a,b.left));
-            return b;
+    static void pushMaxHeap(ArrayList<Integer> heap,int value){
+        heap.add(value);
+        int idx=heap.size()-1;
+        while(idx>0&&heap.get((idx-1)/2)<value){
+            swapNode(heap,(idx-1)/2,idx);
+            idx=(idx-1)/2;
         }
-        a.setRight(merge(a.right,b));
-        return a;
     }
-    static Node removeNode(Node root,int key){
-        if(root==null) return root;
-        if(root.key==key){
-            Node ret=merge(root.left,root.right);
-            root=null;
-            return ret;
+    static void pushMinHeap(ArrayList<Integer> heap,int value){
+        heap.add(value);
+        int idx=heap.size()-1;
+        while(idx>0&&heap.get((idx-1)/2)>value){
+            swapNode(heap,(idx-1)/2,idx);
+            idx=(idx-1)/2;
         }
-        if(key<root.key){
-            root.setLeft((removeNode(root.left,key)));
-        }else{
-            root.setRight((removeNode(root.right,key)));
+    }
+    static int pollMaxHeap(ArrayList<Integer> heap){
+        int ret = heap.get(0);
+        heap.set(0,heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        int cur=0;
+        while(true){
+            int left=cur*2+1, right=cur*2+2;
+            //마지막 리프 도달
+            if(left>=heap.size()) break;
+            int next=cur;
+            //왼쪽 노드로 내려갈 수 있음
+            if(heap.get(next)<heap.get(left)) next=left;
+            //오른쪽 노드로 내려갈 수 있음
+            if(right<heap.size()&&heap.get(next)<heap.get(right)) next=right;
+            //양쪽 노드 모두 내여갈 수 없음
+            if(next==cur) break;
+            swapNode(heap,cur,next);
+            cur=next;
         }
-        return root;
+        return ret;
     }
-    static Node kth(Node root,int k){
-        int leftSize=0;
-        if(root.left!=null) leftSize=root.left.size;
-        if(k<=leftSize) return kth(root.left,k);
-        if(k==leftSize+1) return root;
-        return kth(root.right,k-leftSize-1);
+    static int pollMinHeap(ArrayList<Integer> heap){
+        int ret = heap.get(0);
+        heap.set(0,heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        int cur=0;
+        while(true){
+            int left=cur*2+1, right=cur*2+2;
+            //마지막 리프 도달
+            if(left>=heap.size()) break;
+            int next=cur;
+            //왼쪽 노드로 내려갈 수 있음
+            if(heap.get(next)>heap.get(left)) next=left;
+            //오른쪽 노드로 내려갈 수 있음
+            if(right<heap.size()&&heap.get(next)>heap.get(right)) next=right;
+            //양쪽 노드 모두 내여갈 수 없음
+            if(next==cur) break;
+            swapNode(heap,cur,next);
+            cur=next;
+        }
+        return ret;
     }
-    // static int countOrder(Node root,int key){
-    //     if(root==null) return 0;
-    //     if(root.key>=key) return countOrder(root.left,key);
-    //     int ls=(root.left!=null)?root.left.size:0;
-    //     return ls+1+countOrder(root.right,key);
-    // }
-    //for test
-    // static void showTreat(Node root){
-    //     if(root.left!=null) showTreat(root.left);
-    //     System.out.print(root.key+" ");
-    //     if(root.right!=null) showTreat(root.right);
-    // }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int c=Integer.parseInt(br.readLine());
         while(c-->0){
-            int n=Integer.parseInt(br.readLine());
             StringTokenizer st=new StringTokenizer(br.readLine());
-            int[] log=new int[n];
+            int n=Integer.parseInt(st.nextToken());
+            a=Integer.parseInt(st.nextToken());
+            b=Integer.parseInt(st.nextToken());
+            A=1983;
+            int answer=0;
+            heap1=new ArrayList<Integer>();
+            heap2=new ArrayList<Integer>();
             for(int i=0;i<n;i++){
-                log[i]=Integer.parseInt(st.nextToken());
-            }
-            Node treat=null;
-            for(int i=1;i<=n;i++){
-                treat=addNode(treat,new Node(i));
-            }
-            StringBuilder answer=new StringBuilder();
-            for(int i=n-1;i>=0;i--){
-                int target=kth(treat,n---log[i]).key;
-                treat=removeNode(treat,target);
-                answer.insert(0,target+" ");
+                int a=inputGen();
+                if(heap1.size()==heap2.size()){
+                    pushMaxHeap(heap1,a);
+                }else{
+                    pushMinHeap(heap2,a);
+                }
+                if(heap2.size()>0&&heap1.get(0)>heap2.get(0)){
+                    int temp1=pollMaxHeap(heap1);
+                    int temp2=pollMinHeap(heap2);
+                    pushMinHeap(heap2,temp1);
+                    pushMaxHeap(heap1,temp2);
+                }
+                answer=(answer+heap1.get(0))%20090711;
+
             }
             System.out.println(answer);
         }
