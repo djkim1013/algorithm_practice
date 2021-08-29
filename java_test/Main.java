97e92e90f904eb3473d03a6724ca0096e51c50c6
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-08-30 02:02:16 +0900

    AOJ WORDCHAIN: null pointer exception

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,4 +1,4 @@
-//AOJ: DICTIONARY
+//AOJ: WORDCHAIN
 //2021.08.29
 //category: 깊이우선탐색
 //review:
@@ -8,69 +8,85 @@ import java.io.*;
 
 public class Main {
     static final int M=26;
-    static String[] input;
-    static boolean[][] adj;
-    static boolean[] seen;
-    static ArrayList<Integer> answer;
+    static ArrayList<String>[][] graph;
+    static int[][] adj;
+    static int[] in,out;
     static int n;
-
-    static void makeGraph(){
-        adj=new boolean[M][M];
-        for(int i=1;i<n;i++){
-            String a=input[i-1], b=input[i];
-            int len=Math.min(a.length(),b.length());
-            for(int j=0;j<len;j++){
-                if(a.charAt(j)!=b.charAt(j)){
-                    int front=a.charAt(j)-'a';
-                    int back=b.charAt(j)-'a';
-                    adj[front][back]=true;
-                    break;
-                }
+    static ArrayList<Integer> circuit;
+    static void makeGraph(String[] words){
+        graph=(ArrayList<String>[][])new ArrayList[M][M];
+        adj=new int[M][M];
+        in=new int[M];
+        out=new int[M];
+        for(int i=0;i<n;i++){
+            int a=words[i].charAt(0)-'a';
+            int b=words[i].charAt(words[i].length()-1)-'a';
+            if(graph[a][b]==null)
+                graph[a][b]=new ArrayList<String>();
+            graph[a][b].add(words[i]);
+            adj[a][b]++;
+            out[a]++;
+            in[b]++;
+        }
+    }
+    static void getEulerCircuit(int cur){
+        for(int next=0;next<n;next++){
+            while(adj[cur][next]>0){
+                adj[cur][next]--;
+                getEulerCircuit(next);
+            }
+        }
+        circuit.add(cur);
+    }
+    static void getEulerCircuitOrTrail(){
+        for(int i=0;i<26;i++){
+            if(out[i]==in[i]+1){
+                getEulerCircuit(i);
+                return;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(out[i]>0){
+                getEulerCircuit(i);
+                return;
             }
         }
     }
-    static void dfs(int cur){      
-        seen[cur]=true;
-        for(int next=0;next<M;next++){
-            if(!seen[next]&&adj[cur][next]) dfs(next);
+    static boolean checkEuler(){
+        int plus1=0, minus1=0;
+        for(int i=0;i<26;i++){
+            int del=out[i]-in[i];
+            if(del==1) plus1++;
+            if(del==-1) minus1++;
+            if(del!=0) return false;
         }
-        answer.add(cur);
-    }
-    static void topologicalSort(){
-        seen=new boolean[M];
-        answer=new ArrayList<Integer>();
-        for(int i=0;i<M;i++){
-            if(!seen[i]) dfs(i);
-        }
-        Collections.reverse(answer);
-        for(int i=0;i<M;i++){
-            for(int j=i+1;j<M;j++){
-                if(adj[answer.get(j)][answer.get(i)]){
-                    answer.clear();
-                    return;
-                }
-            }
-        }
+        return (plus1==1&&minus1==1)||(plus1==0&&minus1==0);
     }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int C = Integer.parseInt(br.readLine());
         while(C-->0){
             n = Integer.parseInt(br.readLine());
-            input = new String[n];
+            String[] words=new String[n];
             for(int i=0;i<n;i++){
-                input[i]=br.readLine();
+                words[i]=br.readLine();
             }
-            makeGraph();
-            topologicalSort();
-            if(answer.size()<1){
-                System.out.println("INVALID HYPOTHESIS");
-            }else{
-                StringBuilder output = new StringBuilder();
-                for(int i=0;i<answer.size();i++){
-                    output.append((char)(answer.get(i)+'a'));
+            makeGraph(words);
+            if(!checkEuler())
+                System.out.println("IMPOSSIBLE");
+            else{
+                getEulerCircuitOrTrail();
+                if(circuit.size()==0)
+                    System.out.println("IMPOSSIBLE");
+                else{
+                    StringBuilder answer=new StringBuilder();
+                    for(int i=n-1;i>0;i++){
+                        int a=circuit.get(i-1), b=circuit.get(i);
+                        answer.insert(0,graph[a][b].get(graph[a][b].size()-1));
+                        graph[a][b].remove(graph[a][b].size()-1);
+                    }
+                    System.out.println(answer);;
                 }
-                System.out.println(output);
             }
         }
     }
