c4f402426f5c8cf87006ce474c5a0163dcffc446
djkim1013 <dongjukim1013@gmail.com>
2021-09-26 00:50:01 +0900

    save 0926

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,5 +1,5 @@
-//AOJ:
-//2021.09.24
+//AOJ: GALLERY
+//2021.09.25
 //category: 그래프의 깊이우선탐색
 //review:
 
@@ -7,7 +7,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    static int[][] halls;
+    static int[] galleries;
+
+    static int dfs(int cur){
+        
+        for(int next:halls[cur]){
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int c=Integer.parseInt(br.readLine());
+        while(c-->0){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            int g=Integer.parseInt(st.nextToken());
+            int h=Integer.parseInt(st.nextToken());
+            halls=new int[g][g];
+            galleries=new int[g];
+            Arrays.fill(galleries,-1);
+            for(int i=0;i<h;i++){
+                st=new StringTokenizer(br.readLine());
+                int a=Integer.parseInt(st.nextToken());
+                int b=Integer.parseInt(st.nextToken());
+                halls[a][b]=1;
+                halls[b][a]=1;
+            }
+
+
+
+            
+        }
+        
         
     }
 }
