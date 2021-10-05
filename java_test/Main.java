ad652eab7e6c5385b2bbd44819e5d0142d56c8b2
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-06 01:05:08 +0900

    BOJ  1931: time over

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,4 +1,4 @@
-//BOJ: 11047
+//BOJ: 1931
 //2021.10.05
 //category: 그리디 알고리즘
 //review:
@@ -7,24 +7,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    int[] coins;
+    ArrayList<int[]> adj;
+    int n;
 
-    int countCoins(int n,int rest){
-        if(n==0) return rest;
-        int ret=rest/coins[n];
-        return ret+countCoins(n-1,rest%coins[n]);
+    int countAdj(int idx){
+        if(idx==n) return 1;
+        int ret=countAdj(idx+1);
+        int end=adj.get(idx)[1];
+        int i=idx;
+        while(++i<n)
+            if(adj.get(i)[0]>=end){
+                ret=Math.max(ret,1+countAdj(i));
+                break;
+        }
+        return ret;
     }
 
     public static void main(String[] args) throws Exception {
+        Main mn = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        Main mClass = new Main();
-        mClass.coins = new int[n];
-        for(int i=0;i<n;i++){
-            mClass.coins[i] = Integer.parseInt(br.readLine());
+        mn.adj=new ArrayList<int[]>();
+        mn.n = Integer.parseInt(st.nextToken());
+        for(int i=0;i<mn.n;i++){
+            st=new StringTokenizer(br.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
+            mn.adj.add(new int[]{start,end});
         }
-        System.out.println(mClass.countCoins(n-1,k));
+        Collections.sort(mn.adj,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a,int[] b){
+                if(a[0]==b[0]) return a[1]-b[1];
+                return a[0]-b[0];
+            }
+        });
+        System.out.println(mn.countAdj(0));
     }
 }
