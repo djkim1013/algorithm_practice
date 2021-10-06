848108b20f6e8423a221406fa671dcacfe43f522
djkim1013 <dongjukim1013@gmail.com>
2021-10-06 10:58:20 +0900

    BOJ 1931: time over

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,35 +7,38 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    ArrayList<int[]> adj;
+    int[][] adj;
+    int[] cache;
     int n;
 
     int countAdj(int idx){
         if(idx==n) return 1;
+        if(cache[idx]!=-1) return cache[idx];
         int ret=countAdj(idx+1);
-        int end=adj.get(idx)[1];
+        int end=adj[idx][1];
         int i=idx;
         while(++i<n)
-            if(adj.get(i)[0]>=end){
+            if(adj[i][0]>=end){
                 ret=Math.max(ret,1+countAdj(i));
                 break;
         }
-        return ret;
+        return cache[idx]=ret;
     }
 
     public static void main(String[] args) throws Exception {
         Main mn = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        mn.adj=new ArrayList<int[]>();
         mn.n = Integer.parseInt(st.nextToken());
+        mn.adj=new int[mn.n][2];
+        mn.cache=new int[mn.n];
+        Arrays.fill(mn.cache,-1);
         for(int i=0;i<mn.n;i++){
             st=new StringTokenizer(br.readLine());
-            int start=Integer.parseInt(st.nextToken());
-            int end=Integer.parseInt(st.nextToken());
-            mn.adj.add(new int[]{start,end});
+            mn.adj[i][0]=Integer.parseInt(st.nextToken());
+            mn.adj[i][1]=Integer.parseInt(st.nextToken());
         }
-        Collections.sort(mn.adj,new Comparator<int[]>(){
+        Arrays.sort(mn.adj,new Comparator<int[]>(){
             @Override
             public int compare(int[] a,int[] b){
                 if(a[0]==b[0]) return a[1]-b[1];
