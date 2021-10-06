f28ba5048d495545f5994ea43f685a9dbdb75a97
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-07 00:06:24 +0900

    save

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,16 +7,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int lcm(int[] arr){
+        int n=1;
+        for(int i:arr){
+            while(n<n*i){
+                if(n%i==0) break;
+                n++;
+            }
+        }
+        if(n==arr[arr.length-1]) n*=arr[0];
+        return n;
+    }
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        int answer=1;
         StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr=new int[n];
         for(int i=0;i<n;i++){
-            answer*=Integer.parseInt(st.nextToken());
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-        if(n==1) answer*=answer;
-        System.out.println(answer);
+        Arrays.sort(arr);
+        System.out.println(lcm(arr));
     }
 }
-
