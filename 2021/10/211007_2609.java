c7e00e03d1067728f19df074d0566acb11467f95
djkim1013 <dongjukim1013@gmail.com>
2021-10-07 09:40:00 +0900

    BOJ 2609 faster solution

--- 2021/10/211007_2609.java
+++ 2021/10/211007_2609.java
@@ -13,20 +13,22 @@ public class Main {
         }
         return -1;
     }
-    
+
     static int lcm(int a,int b){
-        for(int n=Math.max(a,b);n<=a*b;n++){
-            if(n%a==0&&n%b==0) return n;
-        }
-        return -1;
+        return b*a/gcf(a,b);
     }
     
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int a=Integer.parseInt(st.nextToken());
-        int b=Integer.parseInt(st.nextToken());
-        System.out.println(gcf(a,b));
-        System.out.println(lcm(a,b));
+        int t=Integer.parseInt(br.readLine());
+        StringTokenizer st=null;
+        StringBuilder answer=new StringBuilder();
+        while(t-->0){
+            st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            answer.append(lcm(a,b)+"\n");
+        }
+        System.out.print(answer);
     }
 }
