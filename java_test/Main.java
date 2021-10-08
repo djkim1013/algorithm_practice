4432794184fc6f8a5c03c5b7c2bb0fd86af12b38
djkim1013 <dongjukim1013@gmail.com>
2021-10-08 14:22:58 +0900

    BOJ 2004 memory over

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,24 +1,52 @@
-//BOJ: 1676
+//BOJ: 2004
 //2021.10.08
 //category: 정수론 및 조합론
 //review:
 
+import java.util.*;
+import java.io.*;
+
 class Main{
-    static public void main(String[] args)throws Exception{
-        int c,n=System.in.read()-'0';
-        while((c=System.in.read())>='0') n=n*10+c-'0';
-        int two=0,five=0;
-        for(int i=2;i<=n;i++){
-            int x=i;
-            while(x%2==0){
-                two++;
-                x/=2;
-            }
-            while(x%5==0){
-                five++;
-                x/=5;
-            }
+    static int[] twoCache,fiveCache;
+
+    static int countFive(int n){
+        if(n<=0) return 0;
+        if(n==5) return 1;
+        if(fiveCache[n]!=-1) return fiveCache[n];
+        if(n%5==0) return fiveCache[n]=countFive(n/5)+1;
+        return fiveCache[n]=0;
+    }
+
+    static int countTwo(int n){
+        if(n<=0) return 0;
+        if(n==2) return 1;
+        if(twoCache[n]!=-1) return twoCache[n];
+        if(n%2==0) return twoCache[n]=countTwo(n/2)+1;
+        return twoCache[n]=0;
+    }
+    
+    public static void main(String[] args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer input=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(input.nextToken());
+        int m=Integer.parseInt(input.nextToken());
+        twoCache=new int[n+1];
+        fiveCache=new int[n+1];
+        Arrays.fill(twoCache,-1);
+        Arrays.fill(fiveCache,-1);
+        int five=0, two=0;
+        for(int i=0;i<=n;i++){
+            five+=countFive(i);
+            two+=countTwo(i);
         }
-        System.out.println(Math.min(two,five));
+        for(int i=0;i<=m;i++){
+            five-=countFive(i);
+            two-=countTwo(i);
+        }
+        for(int i=0;i<=n-m;i++){
+            five-=countFive(i);
+            two-=countTwo(i);
+        }
+        System.out.println(Math.min(five,two));
     }
 }
