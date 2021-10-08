1dae01dae5de869c2fab197b41b8485dd9597486
djkim1013 <dongjukim1013@gmail.com>
2021-10-08 14:30:48 +0900

    BOJ 2004 timeover

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,45 +7,45 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static int[] twoCache,fiveCache;
-
-    static int countFive(int n){
-        if(n<=0) return 0;
-        if(n==5) return 1;
-        if(fiveCache[n]!=-1) return fiveCache[n];
-        if(n%5==0) return fiveCache[n]=countFive(n/5)+1;
-        return fiveCache[n]=0;
-    }
-
     static int countTwo(int n){
-        if(n<=0) return 0;
-        if(n==2) return 1;
-        if(twoCache[n]!=-1) return twoCache[n];
-        if(n%2==0) return twoCache[n]=countTwo(n/2)+1;
-        return twoCache[n]=0;
+        int ret=0;
+        while(n>=2){
+            if(n%2==0){
+                ret++;
+                n/=2;
+            }
+        }
+        return ret;
     }
     
+    static int countFive(int n){
+        int ret=0;
+        while(n>=5){
+            if(n%5==0){
+                ret++;
+                n/=5;
+            }
+        }
+        return ret;
+    }
+
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer input=new StringTokenizer(br.readLine());
         int n=Integer.parseInt(input.nextToken());
         int m=Integer.parseInt(input.nextToken());
-        twoCache=new int[n+1];
-        fiveCache=new int[n+1];
-        Arrays.fill(twoCache,-1);
-        Arrays.fill(fiveCache,-1);
-        int five=0, two=0;
-        for(int i=0;i<=n;i++){
-            five+=countFive(i);
+        int two=0, five=0;
+        for(int i=2;i<=n;i++){
             two+=countTwo(i);
+            five+=countFive(i);
         }
-        for(int i=0;i<=m;i++){
-            five-=countFive(i);
+        for(int i=2;i<=m;i++){
             two-=countTwo(i);
+            five-=countFive(i);
         }
-        for(int i=0;i<=n-m;i++){
-            five-=countFive(i);
+        for(int i=2;i<=n-m;i++){
             two-=countTwo(i);
+            five-=countFive(i);
         }
         System.out.println(Math.min(five,two));
     }
