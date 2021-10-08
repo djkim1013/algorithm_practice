2b4f5d03125337104fa44512a973481c5a9fc0e1
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-08 21:34:36 +0900

    BOJ 1676 another solution

--- 2021yr/10m/211008_1676.java
+++ 2021yr/10m/211008_1676.java
@@ -2,28 +2,20 @@
 //2021.10.08
 //category: 정수론 및 조합론
 //review:
+//      - 팩토리얼의 성질 상 n보다 작은수를 전부 포함하므로 계산 절차를 줄일 수 있다.
+//      - n까지의 자연수 중 5의 배수보다 2의 배수가 항상 많다.
 
 import java.util.*;
 import java.io.*;
 
 class Main{
     static public void main(String[] args)throws Exception{
-        int c,n=System.in.read()-'0';
-        while((c=System.in.read())>='0') n=n*10+c-'0';
-        int[][] twoFive=new int[n+1][2];
-        for(int i=2;i<=n;i++){
-            if(i%2==0){
-                twoFive[i][0]=twoFive[i/2][0]+1;
-            }
-            if(i%5==0){
-                twoFive[i][1]=twoFive[i/5][1]+1;
-            }
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int answer=0;
+        while(n>=5){
+            answer+=n/=5;
         }
-        int two=0,five=0;
-        for(int[] arr:twoFive){
-            two+=arr[0];
-            five+=arr[1];
-        }
-        System.out.println(Math.min(two,five));
+        System.out.println(answer);
     }
 }
