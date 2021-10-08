f9f581aec7507c9df153cb662a0e89d03397abc3
djkim1013 <dongjukim1013@gmail.com>
2021-10-08 14:49:12 +0900

    BOJ 2004 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -13,27 +13,27 @@ class Main{
         int n=Integer.parseInt(input.nextToken());
         int m=Integer.parseInt(input.nextToken());
         int two=0, five=0;
-        int[] cache=new int[n+1];
-        for(int i=2;i<=n;i++){
-            if(i%2==0){
-                cache[i]=cache[i/2]+1;
-                two+=cache[i];
+        for(int i=m;i<=n;i++){
+            int j=i;
+            while(j%2==0){
+                two++;
+                j/=2;
+            }
+            while(j%5==0){
+                five++;
+                j/=5;
             }
         }
-        for(int i=2;i<=n;i++){
-            two-=cache[i];
-            if(i<=n-m) two-=cache[i];
-        }
-        cache=new int[n+1];
-        for(int i=5;i<=n;i++){
-            if(i%5==0){
-                cache[i]=cache[i/2]+1;
-                five+=cache[i];
+        for(int i=2;i<=m;i++){
+            int j=i;
+            while(j%2==0){
+                two--;
+                j/=2;
             }
-        }
-        for(int i=5;i<=n;i++){
-            five-=cache[i];
-            if(i<=n-m) five-=cache[i];
+            while(j%5==0){
+                five--;
+                j/=5;
+            }
         }
         System.out.println(Math.min(five,two));
     }
