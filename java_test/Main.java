83bb47b812997e635a1fbb3efab729a1c3f84426
djkim1013 <dongjukim1013@gmail.com>
2021-10-08 14:44:48 +0900

    BOJ 2004 memory over

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,41 +7,33 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static int countTwo(int n){
-        int ret=0;
-        while(n>=2){
-            if(n%2==0){
-                ret++;
-                n/=2;
-            }
-        }
-        return ret;
-    }
-    
-    static int countFive(int n){
-        int ret=0;
-        while(n>=5){
-            if(n%5==0){
-                ret++;
-                n/=5;
-            }
-        }
-        return ret;
-    }
-
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer input=new StringTokenizer(br.readLine());
         int n=Integer.parseInt(input.nextToken());
         int m=Integer.parseInt(input.nextToken());
         int two=0, five=0;
-        for(int i=m;i<=n;i++){
-            two+=countTwo(i);
-            five+=countFive(i);
+        int[] cache=new int[n+1];
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                cache[i]=cache[i/2]+1;
+                two+=cache[i];
+            }
         }
-        for(int i=2;i<=m;i++){
-            two-=countTwo(i);
-            five-=countFive(i);
+        for(int i=2;i<=n;i++){
+            two-=cache[i];
+            if(i<=n-m) two-=cache[i];
+        }
+        cache=new int[n+1];
+        for(int i=5;i<=n;i++){
+            if(i%5==0){
+                cache[i]=cache[i/2]+1;
+                five+=cache[i];
+            }
+        }
+        for(int i=5;i<=n;i++){
+            five-=cache[i];
+            if(i<=n-m) five-=cache[i];
         }
         System.out.println(Math.min(five,two));
     }
