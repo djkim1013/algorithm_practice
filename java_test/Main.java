fb8de7635b52fc574624284f58b3a6237356d6dc
djkim1013 <dongjukim1013@gmail.com>
2021-10-12 11:16:42 +0900

    BOJ 1874 timeover

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,29 +7,28 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static final String push = "+\n", pop = "-\n", impossible = "NO\n";
-    static String makeSeq(int[] goal){
-        StringBuilder ret = new StringBuilder();
-        Stack<Integer> stack = new Stack<Integer>();
-        int cur=1;
-        for(int i = 0; i < goal.length; i++){
-            while(stack.isEmpty() || stack.peek() < goal[i]){
-                ret.append(push);
-                stack.push(cur++);
+    public static void main(String[] args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int[] arr=new int[n];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> rNum=new Stack<Integer>();
+        StringBuilder answer=new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+            int cur=arr[i];
+            while(!rNum.empty()&&cur>=rNum.peek()){
+                rNum.pop();
             }
-            if(stack.peek()!=goal[i]) return impossible;
-            ret.append(pop);
-            stack.pop();
+            if(rNum.empty()){
+                answer.insert(0,"-1 ");
+            }else{
+                answer.insert(0," ").insert(0,rNum.peek());
+            }
+            rNum.push(cur);
         }
-        return ret.toString();
-    }
-    public static void main( String[] args ) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] goal = new int[n];
-        for(int i = 0; i < n; i++){
-            goal[i] = Integer.parseInt(br.readLine());
-        }
-        System.out.print(makeSeq(goal));
+        System.out.println(answer.toString());
     }
 }
