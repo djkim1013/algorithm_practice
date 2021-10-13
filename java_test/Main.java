0532e9df31add76da634e4db27cf1937c1798336
djkim1013 <dongjukim1013@gmail.com>
2021-10-13 15:48:18 +0900

    BOj 11401 timeover

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,29 +1,23 @@
-//BOJ 1629
+//BOJ 11401
 //2021.10.13
 //category: 분할정복
 //review:
-//      - a^b = a^(b/2) * a^(b/2)
-//      - 메모리 제한에 주의
 
-import java.util.*;
+//import java.util.*;
 import java.io.*;
 
 class Main{
-    static int a,b,c;
-    static int pow(int idx){
-        if(idx==0) return 1;
-        if(idx==1) return a%c;
-        int ret=pow(idx/2);
-        ret=(int)((long)ret*ret%c);
-        if(idx%2>0) ret=(int)((long)ret*a%c);
-        return ret;
+    static final int MOD=1000000007;
+    static int bico(int n,int k){
+        if(k<0||k>n) return 0;
+        if(k==0||k==n) return 1;
+        if(k>n/2) return bico(n,n-k);
+        return (bico(n-1,k)+bico(n-1,k-1))%MOD;
     }
     public static void main(String[] args)throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        a=Integer.parseInt(st.nextToken());
-        b=Integer.parseInt(st.nextToken());
-        c=Integer.parseInt(st.nextToken());
-        System.out.println(pow(b));
+        String input=(new BufferedReader(new InputStreamReader(System.in))).readLine();
+        int n=Integer.parseInt(input.substring(0,input.indexOf(" ")));
+        int k=Integer.parseInt(input.substring(input.indexOf(" ")+1));
+        System.out.println(bico(n,k));
     }
 }
