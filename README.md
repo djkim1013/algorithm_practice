e563f0fc0fcc3bf279cd7813398dc2b94d62741f
djkim1013 <dongjukim1013@gmail.com>
2021-10-13 11:28:10 +0900

    update README

---
+++ README.md
@@ -0,0 +1,2 @@
+AOJ: Algospot (https://www.algospot.com/user/profile/75442)
+BOJ: Baekjoon (https://www.acmicpc.net/user/dongjukim1013)
--- java_test/Main.java
+++ java_test/Main.java
@@ -1,47 +1,14 @@
-//BOJ 5430
+//BOJ 2630
 //2021.10.13
-//category: 큐, 덱
+//category: 분할정복
 //review:
 
 import java.util.*;
 import java.io.*;
 
 class Main{
-    static String run(int[] arr,String command){
-        int order=1;
-        int first=0,last=arr.length;
-        for(int i=0;i<command.length();i++){
-            if(command.charAt(i)=='R'){
-                order*=-1;
-            }else{
-                if(first==last) return "error";
-                if(order>0) first++;
-                else last--;
-            }
-        }
-        arr=Arrays.copyOfRange(arr,first,last);
-        if(order<0){
-            int n=arr.length;
-            for(int i=0;i<n/2;i++){
-                int temp=arr[i];
-                arr[i]=arr[n-i-1];
-                arr[n-i-1]=temp;
-            }
-        }
-        return Arrays.toString(arr).replace(" ","");
-    }
+    
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int t=Integer.parseInt(br.readLine());
-        while(t-->0){
-            String command=br.readLine();
-            int n=Integer.parseInt(br.readLine());
-            StringTokenizer st=new StringTokenizer(br.readLine(),"[,]");
-            int[] arr=new int[n];
-            for(int i=0;i<n;i++){
-                arr[i]=Integer.parseInt(st.nextToken());
-            }
-            System.out.println(run(arr,command));
-        }
     }
 }
