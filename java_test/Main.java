fe0602429e8e906b06082baeaf15d90b3f868d53
djkim1013 <dongjukim1013@gmail.com>
2021-10-13 15:50:30 +0900

    BOJ 11401 memory over

--- java_test/Main.java
+++ java_test/Main.java
@@ -8,16 +8,19 @@ import java.io.*;
 
 class Main{
     static final int MOD=1000000007;
+    static int[][] cache;
     static int bico(int n,int k){
         if(k<0||k>n) return 0;
         if(k==0||k==n) return 1;
         if(k>n/2) return bico(n,n-k);
-        return (bico(n-1,k)+bico(n-1,k-1))%MOD;
+        if(cache[n][k]>0) return cache[n][k];
+        return cache[n][k]=(bico(n-1,k)+bico(n-1,k-1))%MOD;
     }
     public static void main(String[] args)throws Exception{
         String input=(new BufferedReader(new InputStreamReader(System.in))).readLine();
         int n=Integer.parseInt(input.substring(0,input.indexOf(" ")));
         int k=Integer.parseInt(input.substring(input.indexOf(" ")+1));
+        cache=new int[n+1][n/2+1];
         System.out.println(bico(n,k));
     }
 }
