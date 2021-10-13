c39b70e954f434423705f562e1c765718cf1b043
djkim1013 <dongjukim1013@gmail.com>
2021-10-13 14:56:51 +0900

    BOJ 1629 memory over

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,4 +1,4 @@
-//BOJ 1780
+//BOJ 1629
 //2021.10.13
 //category: 분할정복
 //review:
@@ -7,43 +7,16 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static int n;
-    static int[][] paper;
-    static int[] answer=new int[3];
-
-    static void cut(int c,int r,int len){
-        int first=paper[c][r];
-        if(len==1){
-            answer[first+1]++;
-            return;
-        }
-        for(int i=c;i<c+len;i++){
-            for(int j=r;j<r+len;j++){
-                if(paper[i][j]!=first){
-                    len/=3;
-                    for(int k=0;k<3;k++){
-                        for(int l=0;l<3;l++){
-                            cut(c+k*len,r+l*len,len);
-                        }
-                    }
-                    return;
-                }
-            }
-        }
-        answer[first+1]++;
+    static int pow(int a,int b,int c){
+        if(b==1) return a;
+        return (int)((long)a*pow(a,b-1,c)%c);
     }
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        n=Integer.parseInt(br.readLine());
-        paper=new int[n][n];
-        StringTokenizer st;
-        for(int i=0;i<n;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
-                paper[i][j]=Integer.parseInt(st.nextToken());
-            }
-        }
-        cut(0,0,n);
-        for(int i:answer) System.out.println(i);
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        int c=Integer.parseInt(st.nextToken());
+        System.out.println(pow(a,b,c));
     }
 }
