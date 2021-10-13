caefc17124eaf331598d261f640e314c7d9a1a1c
djkim1013 <dongjukim1013@gmail.com>
2021-10-13 15:23:41 +0900

    BOJ 1629 memory over

--- java_test/Main.java
+++ java_test/Main.java
@@ -2,7 +2,7 @@
 //2021.10.13
 //category: 분할정복
 //review:
-//      - a^b = a^(b/2)*a^(b/2)
+//      - a^b = a^(b/2) * a^(b/2)
 
 import java.util.*;
 import java.io.*;
@@ -10,12 +10,12 @@ import java.io.*;
 class Main{
     static int[] cache;
     static int pow(int a,int b,int c){
-        if(cache[b]>0) return cache[b];
-        if(b==0) return cache[b]=1;
-        if(b==1) return cache[b]=a%c;
-        if(b==2) return cache[b]=(int)((long)a*a%c);
-        cache[b]=(int)((long)pow(a,b/2,c)*pow(a,b-b/2,c)%c);
-        return cache[b];
+        int ret=cache[b];
+        if(ret>-1) return ret;
+        if(b==0) return ret=1;
+        if(b==1) return ret=a%c;
+        ret=(int)((long)pow(a,b/2,c)*pow(a,b-b/2,c)%c);
+        return ret;
     }
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -23,6 +23,7 @@ class Main{
         int a=Integer.parseInt(st.nextToken());
         int b=Integer.parseInt(st.nextToken());
         cache=new int[b+1];
+        Arrays.fill(cache,-1);
         int c=Integer.parseInt(st.nextToken());
         System.out.println(pow(a,b,c));
     }
