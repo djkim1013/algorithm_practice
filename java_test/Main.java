88e2b5bc7f15ef317573b12b62ee6223c10fdf6a
djkim1013 <dongjukim1013@gmail.com>
2021-10-14 09:39:47 +0900

    BOJ 11401 time over

--- java_test/Main.java
+++ java_test/Main.java
@@ -12,15 +12,17 @@ class Main{
         int n=Integer.parseInt(input.substring(0,input.indexOf(" ")));
         int k=Integer.parseInt(input.substring(input.indexOf(" ")+1));
         final int MOD=1000000007;
-        int[] cur=new int[n+1];
+        int[] cur=new int[1];
         cur[0]=1;
-        for(int i=1;i<=n;i++){
-            int[] next=new int[i+1];
-            for(int j=0;j<i;j++){
-                next[j]=(cur[j]+cur[j+1])%MOD;
+        for(int i=2;i<=n;i++){
+            int[] next=new int[i/2+1];
+            next[0]=1;
+            for(int j=1;j<cur.length;j++){
+                next[j]=(cur[j]+cur[j-1])%MOD;
             }
+            if(i%2==0) next[next.length-1]=(2*cur[cur.length-1])%MOD;
             cur=next;
         }
-        System.out.println(cur[k]);
+        System.out.println(cur[Math.min(k,n-k-1)]);
     }
 }
