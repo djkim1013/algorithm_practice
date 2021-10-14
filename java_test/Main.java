65b0aabc18acd6ac346698a39ff415c278078b9c
djkim1013 <dongjukim1013@gmail.com>
2021-10-14 11:52:14 +0900

    BOJ 11401 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -2,25 +2,41 @@
 //2021.10.13
 //category: 분할정복
 //review:
+//      - 페르마의 소정리: (a^p)%p = a%p, (a*(a^(p-2))%p)%p = 1%p
+//      => (n!/(k!(n-k)!))%p = (n!%p*(k!(n-k)!)^-1%p)%p
+//                           = (n!%p*(k!(n-k)!)^(p-2)%p)%p
 
 //import java.util.*;
 import java.io.*;
 
 class Main{
+    static final int MOD=1000000007;
+    
+    static long nFactMod(int n){
+        long ret=n;
+        while(n-->1){
+            ret=(ret*n)%MOD;
+        }
+        return (int)ret;
+    }
+
+    static long powMod(long n,int k){
+        if(k==1) return n%MOD;
+        if(k==0) return 1;
+        long ret=powMod(n,k/2)%MOD;
+        ret=ret*ret%MOD;
+        if(k%2==1) ret=ret*n%MOD;
+        return ret;
+    }
+
     public static void main(String[] args)throws Exception{
         String input=(new BufferedReader(new InputStreamReader(System.in))).readLine();
         int n=Integer.parseInt(input.substring(0,input.indexOf(" ")));
         int k=Integer.parseInt(input.substring(input.indexOf(" ")+1));
-        final int MOD=1000000007;
-        int[] arr=new int[(n+1)/2+1];
-        arr[0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=i/2;j>0;j--){
-                arr[j]=(arr[j]+arr[j-1])%MOD;
-            }
-            if(i%2==1) arr[i/2+1]=arr[i/2];
-            //System.out.println(Arrays.toString(arr));
-        }
-        System.out.println(arr[k>n-k?n-k:k]);
+        long answer=nFactMod(k);
+        answer=answer*nFactMod(n-k)%MOD;
+        answer=powMod(answer,MOD-2);
+        answer=answer*nFactMod(n)%MOD;
+        System.out.println(answer);
     }
 }
