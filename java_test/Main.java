75bc16e3209b385c74b00f4283b782dd577d5980
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-16 00:56:14 +0900

    BOJ 6549 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -7,7 +7,38 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+    static int[] arr;
+
+    static int left(int idx){
+        if(idx==0) return 1;
+        int ret=0;
+        for(int i=idx;i>=0;i--){
+            if(arr[i]<arr[idx]) return ret;
+            ret++;
+        }
+        return ret;
+    }
     
+    static int right(int idx){
+        if(idx==arr.length-1) return 0;
+        int ret=0;
+        for(int i=idx+1;i<arr.length;i++){
+            if(arr[i]<arr[idx]) return ret;
+            ret++;
+        }
+        return ret;
+    }
+    
+    static long maxArea(int start,int end){
+        if(start==end) return arr[start];
+        int mid=(start+end)/2;
+        long h=arr[mid];
+        long ret=maxArea(start,mid);
+        ret=Math.max(ret,maxArea(mid+1,end));
+        ret=Math.max(ret,h*(left(mid)+right(mid)));
+        return ret;
+    }
+
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String input;
@@ -16,19 +47,9 @@ class Main{
         while((input=br.readLine()).charAt(0)!='0'){
             st=new StringTokenizer(input);
             int n=Integer.parseInt(st.nextToken());
-            int[] arr=new int[n];
+            arr=new int[n];
             for(int i=0;i<n;i++) arr[i]=Integer.parseInt(st.nextToken());
-            long max=0;
-            Stack<Integer> stk=new Stack<Integer>();
-            for(int i=0;i<n;i++){
-                if(stk.isEmpty()||stk.peek()<=arr[i]){
-                    stk.add(arr[i]);
-                }else{
-                    max=Math.max(max,(long)stk.peek()*stk.size());
-                    stk.add(arr[i]);
-                }
-            }
-            answer.append(max).append("\n");
+            answer.append(maxArea(0,n-1)).append("\n");
         }
         System.out.print(answer);
     }
