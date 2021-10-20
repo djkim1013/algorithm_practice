81d531d99aaf5c0891d42cc60b15783543b3a716
djkim1013 <dongjukim1013@gmail.com>
2021-10-20 15:28:23 +0900

    boj 1654 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -3,54 +3,32 @@
 //category: 이분탐색
 //review:
 //      - 모든 랜선을 사용해야하는 것은 아니다.
+//      - 결국 이분탐색이라는 것은 순차적으로 탐색할 시간을 줄이는 것이 관건이다.
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
 class Main{
+    static long maxLen(Long[] arr,int n,long start,long end){
+        if(start==end) return start-1;
+        long mid=(start+end)/2;
+        int sum=0;
+        for(long i:arr) sum+=i/mid;
+        if(sum<n) return maxLen(arr,n,start,mid);
+        return maxLen(arr,n,mid+1,end);
+    }
+
     static public void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String input=br.readLine();
         int k=Integer.parseInt(input.substring(0,input.indexOf(" ")));
         int n=Integer.parseInt(input.substring(input.indexOf(" ")+1));
-        Integer[] arr=new Integer[k];
+        Long[] arr=new Long[k];
+        long max=0;
         for(int i=0;i<k;i++){
-            arr[i]=Integer.parseInt(br.readLine());
+            arr[i]=Long.parseLong(br.readLine());
+            max=Math.max(max,arr[i]);
         }
-        Arrays.sort(arr);
-        for(int i=arr[k-1];i>=1;i--){
-            int sum=0;
-            int count=i;
-            for(int j=0;j<k;j++){
-                if(arr[j]>=count){
-                    sum+=k-j;
-                    count+=i;
-                    j--;
-                }
-            }
-            if(sum>=n){
-                System.out.println(i);
-                return;
-            }
-        }
+        System.out.println(maxLen(arr,n,1,max));
     }
 }
-
-/* 
-<case 1>
-4 11
-802
-743
-457
-539
-answer : 200
-
-<case 2>
-4 5
-10
-1
-1
-1
-answer : 2
-
-*/
