68e1ab57473b96ff12c966cf07f69541210f357d
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-20 23:27:53 +0900

    boj 1654 faster solution

--- 2021yr/10m/211020_1654.java
+++ 2021yr/10m/211020_1654.java
@@ -6,32 +6,37 @@
 //      - 결국 이분탐색이라는 것은 순차적으로 탐색할 시간을 줄이는 것이 관건이다.
 
 import java.io.*;
-//import java.util.*;
+import java.util.*;
 
 class Main{
-    static int maxLen(Integer[] arr,int n,int start,long end){
-        if(start==end){
-            return start;
-        }
-        int mid=(int)((long)start+end)/2;
+    static int sum(Integer[] arr,int mod){
         int sum=0;
-        for(int i:arr) sum+=i/mid;
-        if(sum<n) return maxLen(arr,n,start,mid);
-        return maxLen(arr,n,mid+1,end);
+        for(int i:arr) sum+=i/mod;
+        return sum;
+    }
+    
+    static int maxLen(Integer[] arr,int max,int n){
+        int start=1, end=max;
+        while(start<end){
+            int mid=(int)(((long)start+end)/2);
+            if(sum(arr,mid)<n) end=mid;
+            else start=mid+1;
+        }
+        return start;
     }
 
     static public void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String input=br.readLine();
-        int k=Integer.parseInt(input.substring(0,input.indexOf(" ")));
-        int n=Integer.parseInt(input.substring(input.indexOf(" ")+1));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int k=Integer.parseInt(st.nextToken());
+        int n=Integer.parseInt(st.nextToken());
         Integer[] arr=new Integer[k];
         int max=0;
         for(int i=0;i<k;i++){
-            arr[i]=Integer.parseInteger(br.readLine());
+            arr[i]=Integer.parseInt(br.readLine());
             max=Math.max(max,arr[i]);
         }
-        int answer=maxLen(arr,n,1,max);
+        int answer=maxLen(arr,max,n);
         if(answer<max) answer--;
         System.out.println(answer);
     }
