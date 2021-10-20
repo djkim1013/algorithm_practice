a5ad0666bbc8ca1b082681236ccbb4cc10aa5d52
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-20 22:25:43 +0900

    save

--- cpp_test/main.cpp
+++ cpp_test/main.cpp
@@ -8,30 +8,8 @@
 using namespace std;
 
 int main(){
-  int C=0;
-  int fence[20000];
-  cin>>C;
-  for (int c=0;c<C;c++){
-    int answer=-1;
-	int n=0;
-	cin>>n;  
-	for(int i=0;i<n;i++){
-	  cin>>fence[i];
-	}
-	for(int i=0;i<n;i++){
-	  int height=fence[i];
-	  int size=height;
-	  for(int j=i-1;j>=0;j--){
-		if(fence[j]<height)break;
-		size+=height;
-	  }
-	  for(int j=i+1;j<n;j++){
-		if(fence[j]<height)break;
-		size+=height;
-	  }
-	  if(answer<size)answer=size;
-	}
-    cout<<answer<<endl;
-  }
-  return 0;
+	int a=0x7fffffff;
+	long long b=1;
+	long long c=(long long)a+1;
+	cout<<c;
 }
