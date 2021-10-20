e030d2be64443a086b20f9eb243650134e59c888
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-20 23:58:13 +0900

    boj 2805 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,4 +1,4 @@
-//BOJ 1654
+//BOJ 2805
 //2021.10.20
 //category: 이분탐색
 //review:
@@ -7,7 +7,38 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+    static long cutLen(int[] trees,int height){
+        long sum=0;
+        for(int i:trees){
+            if(i<=height) continue;
+            sum+=i-height;
+        }
+        return sum;
+    }
+    
+    static int minLen(int[] trees,int max,int m){
+        int start=0, end=max-1;
+        while(start<end){
+            int mid=(int)(((long)start+end)/2);
+            if(cutLen(trees,mid)<(long)m) end=mid;
+            else start=mid+1;
+        }
+        return start;
+    }
+    
     public static void main(String[] args)throws Exception{
-
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        int[] trees=new int[n];
+        st=new StringTokenizer(br.readLine());
+        int max=0;
+        for(int i=0;i<n;i++){
+            trees[i]=Integer.parseInt(st.nextToken());
+            max=Math.max(trees[i],max);
+        }
+        int answer=minLen(trees,max,m);
+        System.out.println(answer>0?answer-1:0);
     }
 }
