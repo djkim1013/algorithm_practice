3120563a8d94e2fca5994b0481a01c3bc1ae8009
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-21 23:40:10 +0900

    boj 1300 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,27 +1,32 @@
-//BOJ 2110
+//BOJ 1300
 //2021.10.21
 //category: 이분탐색
 //review:
+//      - 2805번 문제와 같은 원리로 풀 수 있다.
+//          각 행에서 임의의 수 m 보다 작은 수의 개수는 m/i 와 n 중의 최솟값이다.
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        int k=Integer.parseInt(br.readLine())-1;
-        long half=((long)n*n+n+1)/2;
-        if(k<=half){
-            for(int i=n;i>=1;i--){
-                half-=i;
-                if(half<=k){
-                    int j=k-(int)half;
-                    System.out.println()
-                }
+        int k=Integer.parseInt(br.readLine());
+        int start=1;
+        int end=(int)Math.min((long)n*n,Integer.MAX_VALUE);
+        while(start<end){
+            int mid=(int)((long)start+end)/2;
+            int sum=0;
+            for(int i=1;i<=n;i++){
+                sum+=Math.min(mid/i,n);
             }
-        }else{
-
+            if(sum==k){
+                start=mid;
+                break;
+            }else if(sum<k) start=mid+1;
+            else end=mid-1;
         }
+        System.out.println(start);
     }
 }
