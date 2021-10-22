569af8ddafb4fee3f1d7fa8eba03c2728f8fb66b
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-22 22:12:08 +0900

    boj 11279 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,44 +1,56 @@
-//BOJ 12015
+//BOJ 11279
 //2021.10.22
-//category: 이분탐색
+//category: 우선순위큐
 //review:
-//      - 탐색 중 만들고 있던 부분수열에 해당하는 값이 아니어도 현재까지의 최대 길이 부분수열은 바뀌지 않는 점을 이용한다.
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.util.Arrays;
 
 class Main{
+    static int last=0;
+    static int[] maxHeap;
+
+    static void sort(){
+        int cur=last;
+        while(cur>0){
+            int next=(cur-1)/2;
+            if(maxHeap[next]>=maxHeap[cur]) return;
+            int temp=maxHeap[next];
+            maxHeap[next]=maxHeap[cur];
+            maxHeap[cur]=temp;
+            cur=next;
+        }
+    }
+
+    static void add(int x){
+        maxHeap[last]=x;
+        sort();
+        last++;
+    }
+    
+    static int poll(){
+        if(last==0) return 0;
+        int ret=maxHeap[0];
+        maxHeap[0]=maxHeap[--last];
+        sort();
+        return ret;
+    }
 
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
-        }
-        ArrayList<Integer> part=new ArrayList<Integer>();
-        part.add(arr[0]);
-        for(int i=1;i<n;i++){
-            int t=arr[i];
-            if(part.get(part.size()-1)<t){
-                part.add(t);
+        StringBuilder answer=new StringBuilder();
+        maxHeap=new int[n];
+        while(n-->0){
+            int x=Integer.parseInt(br.readLine());
+            if(x>0){
+                add(x);
             }else{
-                int s=0,e=part.size()-1;
-                while(s<=e){
-                    int m=(s+e)/2;
-                    if(part.get(m)==t){
-                        s=m;
-                        break;
-                    }else if(part.get(m)<t){
-                        s=m+1;
-                    }else{
-                        e=m-1;
-                    }
-                }
-                part.set(s,t);
+                answer.append(poll()).append("\n");
             }
+            //System.out.println(last+" "+Arrays.toString(maxHeap));
         }
-        System.out.println(part.size());
+        System.out.print(answer);
     }
 }
