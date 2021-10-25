cfce484871834d56a4a8a112e54669f8dd28feca
djkim1013 <dongjukim1013@gmail.com>
2021-10-25 15:21:06 +0900

    kitri 01 solved by greedy algorithm

--- 2021yr/10m/211025_kitri01.java
+++ 2021yr/10m/211025_kitri01.java
@@ -6,15 +6,17 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int prev = 1;
-        for(int i:stations){
-            int d=i-w-prev;
-            //이전 기지국 오른쪽 끝과 다음 기지국 왼쪽끝 사이의 거리가 1이상이면 도달거리로 나눈다.
-            if(d>0) answer+=(d+2*w)/(2*w+1);
-            prev=i+w+1;
+        int cur = 1;
+        int next=0;
+        while(cur<=n){
+            if(next<stations.length&&cur>=stations[next]-w){
+                cur=stations[next]+w+1;
+                next++;
+            }else{
+                answer++;
+                cur+=2*w+1;
+            }
         }
-        //마지막 기지국 오른쪽 끝에서 마지막 아파트 사이의 거리가 1이상이면 도달거리로 나눈다.
-        if(n+1-prev>0) answer+=(n+1-prev+2*w)/(2*w+1);
         return answer;
     }
 }
