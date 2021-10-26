3d1ba049bace73dfdd067bea83a169066656ec81
djkim1013 <dongjukim1013@gmail.com>
2021-10-26 13:34:50 +0900

    kitri 05 solved

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,22 +1,21 @@
-//코딩테스트 광탈방지 - 04 시뮬레이션
+//코딩테스트 광탈방지 - 05 해쉬
 //2021.10.26
 //category: 
 //review:
 
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int solution(int[] A, int[] B) {
-        int answer=0;
-        Arrays.sort(A);
-        Arrays.sort(B);
-        int bi=B.length-1;
-        for(int i=A.length-1;i>=0;i--){
-            if(A[i]<B[bi]){
-                bi--;
-                answer++;
-            }
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String,Integer> kinds=new HashMap<String,Integer>();
+        for(String[] c:clothes){
+            if(!kinds.containsKey(c[1])) kinds.put(c[1],0);
+            kinds.put(c[1],kinds.get(c[1])+1);
         }
-        return answer;
+        for(String kind:kinds.keySet()){
+            answer*=kinds.get(kind)+1;
+        }
+        return answer-1;
     }
 }
