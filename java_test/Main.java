14de87f0ede6e32d72a431c1bfdeedb01e993bce
djkim1013 <dongjukim1013@gmail.com>
2021-10-26 14:53:52 +0900

    kitri 06 timeover

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,21 +1,39 @@
-//코딩테스트 광탈방지 - 05 해쉬
+//코딩테스트 광탈방지 - 06 게임맵최단거리
 //2021.10.26
-//category: 
+//category: BFS
 //review:
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String,Integer> kinds=new HashMap<String,Integer>();
-        for(String[] c:clothes){
-            if(!kinds.containsKey(c[1])) kinds.put(c[1],0);
-            kinds.put(c[1],kinds.get(c[1])+1);
+    int[][] maps;
+    int n,m;
+    
+    void searchNext(Queue<int[]> que,int[] target){
+        if(target[0]<0||target[0]>=n) return;
+        if(target[1]<0||target[1]>=m) return;
+        if(maps[target[0]][target[1]]==0) return;
+        que.add(target);
+    }
+    
+    public int solution(int[][] maps) {
+        this.maps=maps;
+        n=maps.length;
+        m=maps[0].length;
+        Queue<int[]> que=new LinkedList<int[]>();
+        que.add(new int[]{0,0,1});
+        while(!que.isEmpty()){
+            int[] cur=que.poll();
+            int i=cur[0],j=cur[1],d=cur[2];
+            if(i==n-1&&j==m-1) return d;
+            d++;
+            searchNext(que,new int[]{i-1,j,d});
+            searchNext(que,new int[]{i+1,j,d});
+            searchNext(que,new int[]{i,j-1,d});
+            searchNext(que,new int[]{i,j+1,d});
+            maps[i][j]=0;
         }
-        for(String kind:kinds.keySet()){
-            answer*=kinds.get(kind)+1;
-        }
-        return answer-1;
+        return -1;
     }
 }
