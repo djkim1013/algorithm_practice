aaedf22e2191ef3a3fc94ff6da58ae22a4b7ce47
djkim1013 <dongjukim1013@gmail.com>
2021-10-26 09:56:40 +0900

    kitri 02 teacher's guide

--- 2021yr/10m/211026_kitri02.java
+++ 2021yr/10m/211026_kitri02.java
@@ -3,17 +3,17 @@
 //category: 정렬
 //review:
 
-import java.util.*;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] strings=new String[numbers.length];
-        for(int i=0;i<numbers.length;i++) strings[i]=numbers[i]+"";
-        //람다식
-        Arrays.sort(strings,(a,b)->(b+a).compareTo(a+b));
-        if(strings[0].charAt(0)=='0') return "0";
-        for(String str:strings) answer+=str;
+        String answer = IntStream.of(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((a,b)->(b+a).compareTo(a+b))
+            .collect(Collectors.joining());
+        if(answer.startsWith("0")) return "0";
         return answer;
     }
 }
