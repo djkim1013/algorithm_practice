3c015bd4d1753ba0dbc7591c48125f7942dbfc5e
djkim1013 <dongjukim1013@gmail.com>
2021-10-26 15:00:01 +0900

    kitri 06 fail

--- java_test/Main.java
+++ java_test/Main.java
@@ -10,11 +10,12 @@ class Solution {
     int[][] maps;
     int n,m;
     
-    void searchNext(Queue<int[]> que,int[] target){
+    void searchNext(Queue<int[]> que,int[] target,int d){
         if(target[0]<0||target[0]>=n) return;
         if(target[1]<0||target[1]>=m) return;
         if(maps[target[0]][target[1]]==0) return;
         que.add(target);
+        maps[target[0]][target[1]]+=d;
     }
     
     public int solution(int[][] maps) {
@@ -22,16 +23,16 @@ class Solution {
         n=maps.length;
         m=maps[0].length;
         Queue<int[]> que=new LinkedList<int[]>();
-        que.add(new int[]{0,0,1});
+        que.add(new int[]{0,0});
         while(!que.isEmpty()){
             int[] cur=que.poll();
-            int i=cur[0],j=cur[1],d=cur[2];
-            if(i==n-1&&j==m-1) return d;
-            d++;
-            searchNext(que,new int[]{i-1,j,d});
-            searchNext(que,new int[]{i+1,j,d});
-            searchNext(que,new int[]{i,j-1,d});
-            searchNext(que,new int[]{i,j+1,d});
+            int i=cur[0],j=cur[1];
+            if(i==n-1&&j==m-1) return maps[i][j];
+            int d=maps[i][j];
+            searchNext(que,new int[]{i-1,j},d);
+            searchNext(que,new int[]{i+1,j},d);
+            searchNext(que,new int[]{i,j-1},d);
+            searchNext(que,new int[]{i,j+1},d);
             maps[i][j]=0;
         }
         return -1;
