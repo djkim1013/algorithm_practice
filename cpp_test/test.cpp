73749f0e67fc3d862db47cb2725614554d0725f1
djkim1013 <dongjukim1013@gmail.com>
2021-10-27 15:43:28 +0900

    save

--- cpp_test/test.cpp
+++ cpp_test/test.cpp
@@ -1,15 +1,33 @@
-//AOJ: 
-//2021.07.
-//문제 분류:
-//해결 분석:
+//BOJ: 1655
+//date: 2021.10.27
+//category: priority queue
+//review:
 
 #include <iostream>
+#include <queue>
+#include <functional>
 
 using namespace std;
 
 int main(){
-	int a=0x7fffffff;
-	int b=1;
-	int c=(int)a+1;
-	cout<<c;
+	int n;
+	cin>>n;
+	priority_queue<int> left;
+	priority_queue<int,vector<int>,less<int>> right;
+	int mid=0;
+	while(n-->0){
+		int input;
+		cin>>input;
+		if(right.size()>left.size()) right.push(input);
+		else left.push(input);
+		while(left.top()>right.top()){
+			int temp=left.top();
+			right.push(temp);
+			left.pop();
+			temp=right.top();
+			left.push(temp);
+			right.pop();
+		}
+		cout<<left.top()<<endl;
+	}
 }
