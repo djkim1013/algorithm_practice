8f3d2a8aaea7836f5747200be9605c2825dda928
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-10-27 23:50:00 +0900

    save

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,34 +1,4 @@
-//BOJ 1655
-//2021.10.27
+//BOJ 11066
+//2021.10.28
 //category: 우선순위큐
 //review:
-
-import java.io.*;
-import java.util.*;
-
-class Main{
-    static public void main(String[] args)throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-        int n=Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> left=new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> right=new PriorityQueue<>();
-        left.add(-10001);
-        right.add(10001);
-        while(n-->0){
-            int input=Integer.parseInt(br.readLine());
-            if(input>left.peek()) right.add(input);
-            else left.add(input);
-            while(right.size()>left.size()){
-                left.add(right.poll());
-            }
-            while(left.size()>right.size()+1){
-                right.add(left.poll());
-            }
-            bw.append(left.peek().toString()).append("\n");
-        }
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-}
