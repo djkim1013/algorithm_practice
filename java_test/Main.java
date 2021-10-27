c5e769fe056758564cf8c6e69cbeb1604eb13efa
djkim1013 <dongjukim1013@gmail.com>
2021-10-27 15:41:30 +0900

    BOJ 1655 solved

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,4 +1,4 @@
-//BOJ 11286
+//BOJ 1655
 //2021.10.27
 //category: 우선순위큐
 //review:
@@ -7,30 +7,21 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static class Number implements Comparable<Number>{
-        int value;
-        @Override
-        public int compareTo(Number that){
-            int a=this.value>0?this.value:-this.value;
-            int b=that.value>0?that.value:-that.value;
-            if(a==b) return this.value-that.value;
-            return a-b;
-        }
-    }
     static public void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         int n=Integer.parseInt(br.readLine());
-        PriorityQueue<Number> priorQ=new PriorityQueue<>();
+        PriorityQueue<Integer> left=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> right=new PriorityQueue<>();
         while(n-->0){
             int input=Integer.parseInt(br.readLine());
-            if(input==0){
-                bw.write((priorQ.isEmpty()?0:priorQ.poll().value)+"\n");
-            }else{
-                Number inputN=new Number();
-                inputN.value=input;
-                priorQ.add(inputN);
+            if(left.size()>right.size()) right.add(input);
+            else left.add(input);
+            while(right.size()>0&&left.peek()>right.peek()){
+                left.add(right.poll());
+                right.add(left.poll());
             }
+            bw.append(left.peek().toString()).append("\n");
         }
         bw.flush();
         br.close();
