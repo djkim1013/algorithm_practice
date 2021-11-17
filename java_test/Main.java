c4ce4e32384ba2cf362767c5b70632ddf96c4bf3
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-11-17 17:23:03 +0900

    save

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 //BOJ 1520
 //2021.11.17
-=======
-//BOJ 11049
-//2021.11.10
->>>>>>> c52bbf7b3bb0d51a4f12bce92e493cc0a421199c
 //category: 동적계획법
 //review:
 
@@ -12,7 +7,6 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-<<<<<<< HEAD
     static int m, n;
     static int[][] map;
     static int[][] nextPath = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
@@ -44,32 +38,6 @@ class Main{
             }
         }
         System.out.println(countPath(0,0,10001));
-=======
-    public static void main(String[] args)throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        int[][] size=new int[n+1][2];
-        int[][] dp=new int[n+1][n+1];
-        for(int i=1;i<=n;i++){
-            String input=br.readLine();
-            size[i][0]=Integer.parseInt(input.substring(0,input.indexOf(" ")));
-            size[i][1]=Integer.parseInt(input.substring(input.indexOf(" ")+1));
-        }
-        for(int len=1;len<n;len++){
-            for(int i=1;i<=n-len;i++){
-                int j=i+len;
-                int temp=Integer.MAX_VALUE;
-                for(int k=i;k<j;k++){
-                    temp=Math.min(
-                        temp,
-                        dp[i][k]+dp[k+1][j]+size[i][0]*size[k][1]*size[j][1]
-                    );
-                }
-                dp[i][j]=temp;
-            }
-        }
-        System.out.println(dp[1][n]);
->>>>>>> c52bbf7b3bb0d51a4f12bce92e493cc0a421199c
         br.close();
     }
 }
