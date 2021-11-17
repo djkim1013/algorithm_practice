d10efb0ee9f7874dbc00a1f77dde570c9b395430
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-11-17 17:21:33 +0900

    boj 1520 time oveer

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,35 +1,43 @@
-//BOJ 11049
-//2021.11.08
+//BOJ 1520
+//2021.11.17
 //category: 동적계획법
 //review:
 
 import java.io.*;
+import java.util.*;
 
 class Main{
-    static int[][] size, cache;
-    static int mpCount(int left,int right){
-        if(left+1==right)
-            return size[left][0]*size[left][1]*size[right][1];
-        if(cache[left][right]!=0) return cache[left][right];
-        int ret=mpCount(left+1,right)+size[left][0]*size[left][1]*size[right][1];
-        ret=Math.min(
-            ret,
-            mpCount(left,right-1)+size[left][0]*size[right][0]*size[right][1]
-        );
-        return cache[left][right]=ret;
+    static int m, n;
+    static int[][] map;
+    static int[][] nextPath = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+    
+    static int countPath(int row,int col,int value){
+        if(row < 0 || row >= m) return 0;
+        if(col < 0 || col >= n) return 0;
+        if(row == m-1 && col == n-1) return 1;
+        if(map[row][col] >= value) return 0;
+        value = map[row][col];
+        int ret = 0;
+        for(int[] next:nextPath){
+            int i = next[0], j = next[1];
+            ret += countPath(row + i, col + j, value);
+        }
+        return ret;
     }
 
     public static void main(String[] args)throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        size=new int[n][2];
-        cache=new int[n][n];
-        for(int i=0;i<n;i++){
-            String input=br.readLine();
-            size[i][0]=Integer.parseInt(input.substring(0,input.indexOf(" ")));
-            size[i][1]=Integer.parseInt(input.substring(input.indexOf(" ")+1));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        map = new int[m][n];
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        System.out.println(mpCount(0,n-1));
+        System.out.println(countPath(0,0,10001));
         br.close();
     }
 }
