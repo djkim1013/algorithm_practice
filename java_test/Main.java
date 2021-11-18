8337c200bfebaa5b7d7de39776610bf320c49df3
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-11-19 02:47:04 +0900

    boj 2629 solved

--- java_test/Main.java
+++ java_test/Main.java
@@ -1,5 +1,5 @@
-//BOJ 10942
-//2021.11.17
+//BOJ 2629
+//2021.11.19
 //category: 동적계획법
 //review:
 
@@ -7,34 +7,51 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static int[] arr;
+    static int n;
+    static int[] weights;
     static int[][] dp;
 
-    static int judgePalindrome(int left, int right){
-        if(left >= right) return 1;
-        if(dp[left][right] != -1) return dp[left][right];
-        if(arr[left] != arr[right]) return dp[left][right] = 0;
-        return dp[left][right] = judgePalindrome(left + 1, right - 1);
+    static int combination(int idx, int remain){
+        if(remain == 15000){
+            return 1;
+        }
+        if(idx == n || remain > 30000){
+            return 0;
+        }
+        if(dp[idx][remain] != -1){
+            return dp[idx][remain];
+        }
+        int ret = combination(idx + 1, remain);
+        ret |= combination(idx + 1, remain + weights[idx]);
+        if(remain >= weights[idx]) ret |= combination(idx + 1, remain - weights[idx]);
+        return dp[idx][remain] = ret;
     }
-
+    
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
-        dp = new int[n + 1][n + 1];
+        
+        n = Integer.parseInt(br.readLine());
+        weights = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            Arrays.fill(dp[i], -1);
+        for(int i = 0; i < n; i++){
+            weights[i] = Integer.parseInt(st.nextToken());
         }
+        
         int m = Integer.parseInt(br.readLine());
-        while(m-- > 0){
-            st = new StringTokenizer(br.readLine());
-            int left = Integer.parseInt(st.nextToken());
-            int right = Integer.parseInt(st.nextToken());
-            bw.write(judgePalindrome(left,right)+"\n");
+        int[] beads = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++){
+            beads[i] = Integer.parseInt(st.nextToken());
         }
+        
+        dp = new int[n][30001];
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        for(int bead : beads){
+            bw.write(combination(0, bead + 15000) == 1 ? "Y " : "N ");
+        }
+        
         bw.flush();
         bw.close();
         br.close();
