7312dea9ee0e26f2c9d9003a0095c131228ac58e
djkim1013 <52436846+djkim1013@users.noreply.github.com>
2021-11-21 19:33:28 +0900

    boj 2293 remove recursion

--- solved/dynamic_programming/211121_2293.java
+++ solved/dynamic_programming/211121_2293.java
@@ -4,26 +4,8 @@
 //review:
 
 import java.io.*;
-import java.util.*;
 
 class Main{
-    static int[][] dp;
-
-    static int dfs(int[] coins,int idx,int k){
-        if(k == 0) return 1;
-        if(idx == coins.length) return 0;
-        int ret = dp[idx][k];
-        if(ret != -1) return ret;
-        ret = 0;
-        int value = coins[idx];
-        int remain = k;
-        while(remain>=0){
-            ret += dfs(coins,idx+1,remain);
-            remain-=value;
-        }
-        return dp[idx][k] = ret;
-    }
-
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
@@ -31,9 +13,15 @@ class Main{
         int k = Integer.parseInt(input.substring(input.indexOf(" ")+1));
         int[] coins = new int[n];
         for(int i=0;i<n;i++) coins[i] = Integer.parseInt(br.readLine());
-        dp = new int[n][k+1];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        Arrays.sort(coins);
-        System.out.println(dfs(coins,0,k));
+        int[] dp = new int[k+1];
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            int value = coins[i];
+            for(int j=1;j<=k;j++){
+                if(j>=value) dp[j] += dp[j-value];
+            }
+        }
+        System.out.println(dp[k]);
+        br.close();
     }
 }
